1acbe0f58ac6be658c5b6f758d737578c4962ee6
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2014-12-12 15:05:03 +0000

    updated

---
+++ src/net/masterthought/dlanguage/DLanguageHighlighterFactory.java
@@ -0,0 +1,18 @@
+package net.masterthought.dlanguage;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+
+public class DLanguageHighlighterFactory extends SyntaxHighlighterFactory {
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new DLanguageSyntaxHighlighter();
+    }
+
+}
