f3aa2ba6d508a9262f776152528b0020d1e90b0c
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2016-01-17 19:02:22 +0000

    tidy up importer

--- src/net/masterthought/dlanguage/project/DubProjectImportBuilder.java
+++ src/net/masterthought/dlanguage/project/DubProjectImportBuilder.java
@@ -16,18 +16,14 @@ import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.Pair;
 import com.intellij.packaging.artifacts.ModifiableArtifactModel;
 import com.intellij.projectImport.ProjectImportBuilder;
-import gnu.trove.THashSet;
 import net.masterthought.dlanguage.DLanguageIcons;
 import net.masterthought.dlanguage.DLanguageSdkType;
 import net.masterthought.dlanguage.module.DLanguageDubModuleBuilder;
-import net.masterthought.dlanguage.module.DLanguageModuleBuilder;
-import net.masterthought.dlanguage.module.DLanguageModuleType;
 import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -84,7 +80,7 @@ public class DubProjectImportBuilder extends ProjectImportBuilder<String> {
 
     @Override
     public boolean isMarked(String s) {
-      return getList().contains(s);
+        return getList().contains(s);
     }
 
 
@@ -94,7 +90,7 @@ public class DubProjectImportBuilder extends ProjectImportBuilder<String> {
         final List<Module> modules = new ArrayList<>();
         final Project myProject = project;
         ModifiableModuleModel moduleModelCandidate = modifiableModuleModel;
-        if(moduleModelCandidate == null){
+        if (moduleModelCandidate == null) {
             moduleModelCandidate = ModuleManager.getInstance(project).getModifiableModel();
         }
         final ModifiableModuleModel moduleModel = moduleModelCandidate;
@@ -102,14 +98,13 @@ public class DubProjectImportBuilder extends ProjectImportBuilder<String> {
             @Override
             public void run() {
                 commitSdk(myProject);
-//                setProjectSettings(myProject, dubPath, dubJsonPath);
                 modules.addAll(buildModules(myProject, moduleModel));
             }
         });
         return modules;
     }
 
-    private List<Module> buildModules(Project project, ModifiableModuleModel moduleModel){
+    private List<Module> buildModules(Project project, ModifiableModuleModel moduleModel) {
         List<Module> moduleList = new ArrayList<>();
         DubConfigurationParser dubConfigurationParser = new DubConfigurationParser(project.getBaseDir().getCanonicalPath());
         DubConfigurationParser.DubPackage pkg = dubConfigurationParser.getDubPackage();
@@ -129,18 +124,18 @@ public class DubProjectImportBuilder extends ProjectImportBuilder<String> {
         return moduleList;
     }
 
-     private void commitSdk(Project project){
-         ProjectRootManager.getInstance(project).setProjectSdk(findOrCreateSdk());
-     }
+    private void commitSdk(Project project) {
+        ProjectRootManager.getInstance(project).setProjectSdk(findOrCreateSdk());
+    }
 
-    private Sdk findOrCreateSdk(){
+    private Sdk findOrCreateSdk() {
         final DLanguageSdkType sdkType = DLanguageSdkType.getInstance();
 
         Comparator<Sdk> sdkComparator = new Comparator<Sdk>() {
             public int compare(Sdk s1, Sdk s2) {
-                if(s1.getSdkType() == sdkType){
+                if (s1.getSdkType() == sdkType) {
                     return -1;
-                } else if(s2.getSdkType() == sdkType){
+                } else if (s2.getSdkType() == sdkType) {
                     return 1;
                 } else {
                     return 0;
@@ -149,225 +144,4 @@ public class DubProjectImportBuilder extends ProjectImportBuilder<String> {
         };
         return SdkConfigurationUtil.findOrCreateSdk(sdkComparator, sdkType);
     }
-
-//     private void setProjectSettings(Project project){
-//         DLanguageBuildSettings buildSettings = DLanguageBuildSettings.getInstance(project);
-//
-//     }
-
-    //private def setProjectSettings(
-//        project: Project,
-//        stackPath: String,
-//        stackYamlPath: String): Unit = {
-//        val buildSettings = HaskellBuildSettings.getInstance(project)
-//        buildSettings.setUseCabal(false)
-//        buildSettings.setUseStack(true)
-//        buildSettings.setStackPath(stackPath)
-//        buildSettings.setStackFile(stackYamlPath)
-//        }
-
-    //private def commitSdk(project: Project): Unit = {
-//        ProjectRootManager.getInstance(project).setProjectSdk(findOrCreateSdk())
-//        }
-//
-//private def findOrCreateSdk(): Sdk = {
-//        val sdkType = HaskellSdkType.getInstance
-//        // Essentially, sorts the Sdks so that the Haskell one comes first.
-//        // If it doesn't exist, should create a new one.
-//        val cmp = { (sdk1: Sdk, sdk2: Sdk) =>
-//        if (sdk1.getSdkType == sdkType) -1
-//        else if (sdk2.getSdkType == sdkType) 1
-//        else 0
-//        }
-//        SdkConfigurationUtil.findOrCreateSdk(cmp, sdkType)
-//        }
-//
-//        def getImportRoot: String = {
-//        val path = getFileToImport
-//        val f = new File(path)
-//        if (f.isDirectory) path else f.getParent
-//        }
-//        }
-
-    //private def buildModule(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        moduleDir: String,
-//        moduleName: String): Module = {
-//        val moduleFilePath = FileUtil.join(moduleDir, s"$moduleName.iml")
-//        // TODO: Parse the cabal file to determine the appropriate module name, source/test dirs, etc.
-//        val moduleBuilder = HaskellModuleType.getInstance.createModuleBuilder()
-//        moduleBuilder.setModuleFilePath(moduleFilePath)
-//        moduleBuilder.setContentEntryPath(moduleDir)
-//        moduleBuilder.setName(moduleName)
-//        moduleBuilder.addSourcePath(Pair.create(moduleDir, ""))
-//        val module = moduleBuilder.createModule(moduleModel)
-//        moduleBuilder.commit(project)
-//        module
-//        }
-
-
-//    private def buildStackPackageModule(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        projectRoot: String,
-//        pkg: StackYaml.Package): (String, Module) = {
-//        val moduleDir = FileUtil.join(projectRoot, pkg.path)
-//        // This should already be validated.
-//        val cabalFile = StackYamlUtil.unsafeFindCabalFile(projectRoot, pkg)
-//        val moduleName = cabalFile.getName.split('.').head
-//        val module = buildModule(project, moduleModel, moduleDir, moduleName)
-//        (moduleDir, module)
-//        }
-
-//    /private def buildModules(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        stackYaml: StackYaml): util.List[Module] = {
-//        val projectRoot = getImportRoot
-//        val (moduleDirs, modules) = stackYaml.packages.collect { case pkg if isMarked(pkg) =>
-//        buildStackPackageModule(project, moduleModel, projectRoot, pkg)
-//        }.unzip
-//        // If we aren't updating an existing project AND the project root isn't a module,
-//        // let's create it as the "root" module.
-//        if (!isUpdate && !moduleDirs.contains(projectRoot)) {
-//        val projectName = new File(projectRoot).getName
-//        val projectModule = buildModule(project, moduleModel, projectRoot, projectName + " (root)")
-//        modules.add(projectModule)
-//        }
-//        modules
-//        }
-
-
-
 }
-//
-///**
-// * Imports a Stack project and configures modules from the stack.yaml file.
-// */
-//class DubProjectImportBuilder extends ProjectImportBuilder[StackYaml.Package] {
-//        override def getName: String = "Stack"
-//        override def getIcon: Icon = HaskellIcons.FILE
-//
-//        sealed case class Params(
-//        var stackPath: Option[String] = None,
-//        var stackYamlPath: Option[String] = None,
-//        var packages: util.List[StackYaml.Package] = util.Collections.emptyList(),
-//        var openSettings: Boolean = false
-//        )
-//
-//        val params = Params()
-//
-//        override def getList: util.List[StackYaml.Package] = params.packages
-//        override def setList(list: util.List[StackYaml.Package]): Unit = params.packages = list
-//        override def isOpenProjectSettingsAfter: Boolean = params.openSettings
-//        override def setOpenProjectSettingsAfter(on: Boolean): Unit = params.openSettings = on
-//        override def isMarked(element: StackYaml.Package): Boolean = getList.contains(element)
-//
-//        override def commit(
-//        project: Project,
-//        model: ModifiableModuleModel,
-//        modulesProvider: ModulesProvider,
-//        artifactModel: ModifiableArtifactModel): util.List[Module] = {
-//        val stackPath = unsafeGetParam(_.stackPath, "stackPath")
-//        val stackYamlPath = unsafeGetParam(_.stackYamlPath, "stackYamlPath")
-//        val stackYaml = StackYaml.unsafeFromFile(stackYamlPath)
-//        val moduleModel = Option(model).getOrElse {
-//        ModuleManager.getInstance(project).getModifiableModel
-//        }
-//        ApplicationManager.getApplication.runWriteAction {
-//        commitSdk(project)
-//        setProjectSettings(project, stackPath, stackYamlPath)
-//        buildModules(project, moduleModel, stackYaml)
-//        }
-//        }
-//
-//private def setProjectSettings(
-//        project: Project,
-//        stackPath: String,
-//        stackYamlPath: String): Unit = {
-//        val buildSettings = HaskellBuildSettings.getInstance(project)
-//        buildSettings.setUseCabal(false)
-//        buildSettings.setUseStack(true)
-//        buildSettings.setStackPath(stackPath)
-//        buildSettings.setStackFile(stackYamlPath)
-//        }
-//
-//private def buildModules(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        stackYaml: StackYaml): util.List[Module] = {
-//        val projectRoot = getImportRoot
-//        val (moduleDirs, modules) = stackYaml.packages.collect { case pkg if isMarked(pkg) =>
-//        buildStackPackageModule(project, moduleModel, projectRoot, pkg)
-//        }.unzip
-//        // If we aren't updating an existing project AND the project root isn't a module,
-//        // let's create it as the "root" module.
-//        if (!isUpdate && !moduleDirs.contains(projectRoot)) {
-//        val projectName = new File(projectRoot).getName
-//        val projectModule = buildModule(project, moduleModel, projectRoot, projectName + " (root)")
-//        modules.add(projectModule)
-//        }
-//        modules
-//        }
-//
-///**
-// * Builds an intellij module for a given package.  Must be run within a write action.
-// */
-//private def buildStackPackageModule(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        projectRoot: String,
-//        pkg: StackYaml.Package): (String, Module) = {
-//        val moduleDir = FileUtil.join(projectRoot, pkg.path)
-//        // This should already be validated.
-//        val cabalFile = StackYamlUtil.unsafeFindCabalFile(projectRoot, pkg)
-//        val moduleName = cabalFile.getName.split('.').head
-//        val module = buildModule(project, moduleModel, moduleDir, moduleName)
-//        (moduleDir, module)
-//        }
-//
-//private def buildModule(
-//        project: Project,
-//        moduleModel: ModifiableModuleModel,
-//        moduleDir: String,
-//        moduleName: String): Module = {
-//        val moduleFilePath = FileUtil.join(moduleDir, s"$moduleName.iml")
-//        // TODO: Parse the cabal file to determine the appropriate module name, source/test dirs, etc.
-//        val moduleBuilder = HaskellModuleType.getInstance.createModuleBuilder()
-//        moduleBuilder.setModuleFilePath(moduleFilePath)
-//        moduleBuilder.setContentEntryPath(moduleDir)
-//        moduleBuilder.setName(moduleName)
-//        moduleBuilder.addSourcePath(Pair.create(moduleDir, ""))
-//        val module = moduleBuilder.createModule(moduleModel)
-//        moduleBuilder.commit(project)
-//        module
-//        }
-//
-//// Helper to be called only after validation.
-//private def unsafeGetParam[A](f: Params => Option[A], name: String): A = {
-//        f(params).getOrElse { throw new RuntimeException(s"Parameter '$name' not set.") }
-//        }
-//
-//private def commitSdk(project: Project): Unit = {
-//        ProjectRootManager.getInstance(project).setProjectSdk(findOrCreateSdk())
-//        }
-//
-//private def findOrCreateSdk(): Sdk = {
-//        val sdkType = HaskellSdkType.getInstance
-//        // Essentially, sorts the Sdks so that the Haskell one comes first.
-//        // If it doesn't exist, should create a new one.
-//        val cmp = { (sdk1: Sdk, sdk2: Sdk) =>
-//        if (sdk1.getSdkType == sdkType) -1
-//        else if (sdk2.getSdkType == sdkType) 1
-//        else 0
-//        }
-//        SdkConfigurationUtil.findOrCreateSdk(cmp, sdkType)
-//        }
-//
-//        def getImportRoot: String = {
-//        val path = getFileToImport
-//        val f = new File(path)
-//        if (f.isDirectory) path else f.getParent
-//        }
-//        }
--- src/net/masterthought/dlanguage/project/DubProjectImportProvider.java
+++ src/net/masterthought/dlanguage/project/DubProjectImportProvider.java
@@ -1,13 +1,10 @@
 package net.masterthought.dlanguage.project;
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.ProjectJdkForModuleStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
-import net.masterthought.dlanguage.DLanguageSdkType;
-import net.masterthought.dlanguage.module.DubInitForModuleStep;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -23,23 +20,6 @@ public class DubProjectImportProvider extends ProjectImportProvider {
     @Override
     public ModuleWizardStep[] createSteps(WizardContext wizardContext) {
         return ModuleWizardStep.EMPTY_ARRAY;
-//        List<ModuleWizardStep> steps = new ArrayList<>();
-//
-////        ModuleWizardStep setCompiler = new ProjectJdkForModuleStep(wizardContext, DLanguageSdkType.getInstance()) {
-////            public void updateDataModel() {
-////                super.updateDataModel();
-////                moduleBuilder.setModuleJdk(getJdk());
-////            }
-////        };
-//        ModuleWizardStep setDubInit = new DubInitForModuleStep(wizardContext);
-//
-////        steps.add(setCompiler);
-//
-////        if((moduleBuilder.getBuilderId() != null && moduleBuilder.getBuilderId().equals("DLangDubApp"))){
-//            steps.add(setDubInit);
-////        }
-//
-//        return steps.toArray(new ModuleWizardStep[steps.size()]);
     }
 
     @Override
@@ -65,66 +45,4 @@ public class DubProjectImportProvider extends ProjectImportProvider {
     public boolean canImportFromFile(VirtualFile file) {
         return true;
     }
-
-//    override def canImportFromFile(file: VirtualFile): Boolean = {
-//        try {
-//        val content = new String(file.contentsToByteArray(), CharsetToolkit.UTF8)
-//        // Bail out if we can't parse the yaml file.
-//        if (StackYaml.fromString(content).isLeft) return false
-//        // If we can parse it, go ahead and update the builder accordingly.
-//        builder.params.stackYamlPath = Some(file.getCanonicalPath)
-//        true
-//        } catch {
-//        // If we can't read the file, we can't import from it.
-//        case e: java.io.IOException =>
-//        false
-//        }
-//        }
 }
-
-
-
-///**
-// * Provides the UI for importing a Stack project.
-// */
-//class StackProjectImportProvider(builder: StackProjectImportBuilder)
-//        extends ProjectImportProvider(builder) {
-//        override def createSteps(context: WizardContext): Array[ModuleWizardStep] = Array(
-//        new StackProjectImportStep(context),
-//        new StackSelectImportedProjectsStep(context)
-//        )
-//
-//        /**
-//         * Checks to see if a given file or directory can be imported as a Stack project.
-//         */
-//        override def canImport(fileOrDirectory: VirtualFile, project: Project): Boolean = {
-//        // If we're not importing a directory, validate it as a file.
-//        if (!fileOrDirectory.isDirectory) return canImportFromFile(fileOrDirectory)
-//        for (stackFile <- Option(fileOrDirectory.findChild("stack.yaml"))) {
-//        if (canImportFromFile(stackFile)) return true
-//        }
-//        val stackFiles = fileOrDirectory.getChildren.filter(f =>
-//        !f.isDirectory && f.getName != "stack.yaml"
-//        )
-//        stackFiles.exists(canImportFromFile)
-//        }
-//
-//        /**
-//         * Confirms that the Stack config file to be imported is valid
-//         * and, if so, set the builder's parameters accordingly.
-//         */
-//        override def canImportFromFile(file: VirtualFile): Boolean = {
-//        try {
-//        val content = new String(file.contentsToByteArray(), CharsetToolkit.UTF8)
-//        // Bail out if we can't parse the yaml file.
-//        if (StackYaml.fromString(content).isLeft) return false
-//        // If we can parse it, go ahead and update the builder accordingly.
-//        builder.params.stackYamlPath = Some(file.getCanonicalPath)
-//        true
-//        } catch {
-//        // If we can't read the file, we can't import from it.
-//        case e: java.io.IOException =>
-//        false
-//        }
-//        }
-//        }
