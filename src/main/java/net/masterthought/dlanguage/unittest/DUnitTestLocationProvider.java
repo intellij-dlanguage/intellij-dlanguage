cac3ce9c3e1e30e2f54dc6daa166aaf5dd632924
SingingBush <singingbush@hotmail.com>
2016-12-15 00:27:41 +0000

    merged in changes from trespasserw. See #114

--- src/main/java/net/masterthought/dlanguage/unittest/DUnitTestLocationProvider.java
+++ src/main/java/net/masterthought/dlanguage/unittest/DUnitTestLocationProvider.java
@@ -1,31 +1,38 @@
 package net.masterthought.dlanguage.unittest;
 
 import com.intellij.execution.Location;
+import com.intellij.execution.testframework.sm.runner.SMTestLocator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.testIntegration.TestLocationProvider;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class DUnitTestLocationProvider implements TestLocationProvider
+public class DUnitTestLocationProvider implements SMTestLocator
 {
+    private static final DUnitTestLocationProvider INSTANCE = new DUnitTestLocationProvider();
+
     public static final String PROTOCOL_ID = "D";
     public static final String PROTOCOL_PREFIX = PROTOCOL_ID + "://";
 
+    public static DUnitTestLocationProvider getInstance() {
+        return INSTANCE;
+    }
+
     @NotNull
     @Override
-    public List<Location> getLocation(@NotNull String protocolId, @NotNull String locationData, Project project)
+    public List<Location> getLocation(@NotNull String protocol, @NotNull String path, @NotNull Project project, @NotNull GlobalSearchScope scope)
     {
-        List<Location> locations = new LinkedList<Location>();
+        final List<Location> locations = new LinkedList<Location>();
 
-        if (!StringUtil.equals(PROTOCOL_ID, protocolId) || StringUtil.isEmpty(locationData))
+        if (!StringUtil.equals(PROTOCOL_ID, protocol) || StringUtil.isEmpty(path))
         {
             return locations;
         }
 
-        // TODO: Process the location data to find the corresponding source
+        // TODO: Process the path to find the corresponding source
 
         return locations;
     }
