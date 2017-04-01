58d4298390cd4da891df184453c1c1a20c96b8aa
singingbush <singingbush@hotmail.com>
2017-04-01 13:42:51 +0100

    issue #150 - changes to DubConfigurationParser

--- src/main/java/net/masterthought/dlanguage/project/DubConfigurationParser.java
+++ src/main/java/net/masterthought/dlanguage/project/DubConfigurationParser.java
@@ -1,9 +1,7 @@
 package net.masterthought.dlanguage.project;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.ParametersList;
@@ -12,43 +10,52 @@ import com.intellij.execution.process.ProcessAdapter;
 import com.intellij.execution.process.ProcessEvent;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Key;
 
 import java.io.File;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class DubConfigurationParser {
 
-    private static final Logger LOG = Logger.getInstance("#" + DubConfigurationParser.class.getName());
+    private static final Logger LOG = Logger.getInstance(DubConfigurationParser.class);
 
     private JsonObject dubConfiguration;
-    private Project project;
-    private String dubBinaryPath;
+    private final Project project;
+    private final String dubBinaryPath;
 
     public DubConfigurationParser(Project project, String dubBinaryPath) {
-
         this.project = project;
         this.dubBinaryPath = dubBinaryPath;
-        this.dubConfiguration = parseDubConfiguration();
+        parseDubConfiguration()
+            .ifPresent(jsonObject -> dubConfiguration = jsonObject);
     }
 
     public class DubPackage {
         public String name;
         public String path;
-        public String sourcesDir;
+        public List<String> dependencies;
+        public String sourcesDir; // todo: this should prob be String[]
+        public List<String> resources;
         public String version;
         public boolean isRootPackage;
 
-        public DubPackage(String name, String path, String sourcesDir, String version, boolean isRootPackage) {
+        public DubPackage(String name, String path, List<String> dependencies, String sourcesDir, List<String> resources, String version, boolean isRootPackage) {
             this.name = name;
             this.path = path;
+            this.dependencies = dependencies;
             this.sourcesDir = sourcesDir;
+            this.resources = resources;
             this.version = version;
             this.isRootPackage = isRootPackage;
         }
     }
 
+    // todo: make this return Optional<DubPackage>
     public DubPackage getDubPackage() {
         for (DubPackage dubPackage : getAllPackages()) {
             if (dubPackage.isRootPackage) {
@@ -58,6 +65,11 @@ public class DubConfigurationParser {
         return null;
     }
 
+    /**
+     *
+     * @return a list of DubPackage that the root DubPackage depends on. These may be sub-packages, libs, or other dub packages
+     */
+    // todo: make this return Optional<List<DubPackage>>
     public List<DubPackage> getDubPackageDependencies() {
         List<DubPackage> dependencies = new ArrayList<>();
         for (DubPackage dubPackage : getAllPackages()) {
@@ -69,59 +81,95 @@ public class DubConfigurationParser {
     }
 
     private List<DubPackage> getAllPackages() {
-        JsonArray packages = dubConfiguration.get("packages").getAsJsonArray();
-        String rootPackage = dubConfiguration.get("rootPackage").getAsString();
-        List<DubPackage> packageList = new ArrayList<>();
+        if(dubConfiguration == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        final JsonArray packages = dubConfiguration.get("packages").getAsJsonArray();
+        final String rootPackage = dubConfiguration.get("rootPackage").getAsString();
+
+        final Gson gson = new Gson();
+        final Type listString = new TypeToken<List<String>>() {}.getType();
+
+        List<DubPackage> packageList = new ArrayList<>(packages.size());
         for (JsonElement pkg : packages) {
             JsonObject thePackage = ((JsonObject) pkg);
             String path = thePackage.get("path").getAsString();
             String name = thePackage.get("name").getAsString();
+            final List<String> dependencies = gson.fromJson(thePackage.get("dependencies").getAsJsonArray(), listString);
             String version = thePackage.get("version").getAsString();
             String sourcesDir = "source";
             JsonArray importPaths = thePackage.get("importPaths").getAsJsonArray();
             if (importPaths.size() > 0) {
                 sourcesDir = importPaths.get(0).getAsString();
             }
-            packageList.add(new DubPackage(name, path, sourcesDir, version, name.equals(rootPackage)));
+            final List<String> stringImportPaths = gson.fromJson(thePackage.get("stringImportPaths").getAsJsonArray(), listString); // eg: "views"
+            packageList.add(new DubPackage(name, path, dependencies, sourcesDir, stringImportPaths, version, name.equals(rootPackage)));
         }
         return packageList;
     }
 
-    private JsonObject parseDubConfiguration() {
+    private Optional<JsonObject> parseDubConfiguration() {
         try {
-            String baseDir = project.getBaseDir().getCanonicalPath();
-            GeneralCommandLine commandLine = new GeneralCommandLine();
+            final String baseDir = project.getBaseDir().getCanonicalPath();
+            final GeneralCommandLine commandLine = new GeneralCommandLine();
             commandLine.setWorkDirectory(new File(baseDir));
             commandLine.setExePath(dubBinaryPath);
-            ParametersList parametersList = commandLine.getParametersList();
+            final ParametersList parametersList = commandLine.getParametersList();
             parametersList.addParametersString("describe");
 
-            final OSProcessHandler process = new OSProcessHandler(commandLine.createProcess(), commandLine.getCommandLineString());
+            final String dubCommand = commandLine.getCommandLineString();
+            final OSProcessHandler process = new OSProcessHandler(commandLine.createProcess(), dubCommand);
 
             final StringBuilder builder = new StringBuilder();
+            final List<String> errors = new ArrayList<>();
+
             process.addProcessListener(new ProcessAdapter() {
                 @Override
                 public void onTextAvailable(ProcessEvent event, Key outputType) {
-                    builder.append(event.getText());
+                    switch (outputType.toString()) {
+                        case "stdout":
+                            builder.append(event.getText());
+                            break;
+                        case "stderr":
+                            errors.add(event.getText());
+                            break;
+                    }
                 }
             });
 
             process.startNotify();
             process.waitFor();
 
-            // remove the warning line at the top if it exists
-            final String json = builder.toString()
-                .replaceAll("WARNING.+", "")
-                .replace(commandLine.getCommandLineString(), "")
-                .trim();
+            final Integer exitCode = process.getExitCode();
 
-            // process output of dub describe into jsonObject
-            return new JsonParser().parse(json).getAsJsonObject();
-
-        } catch (ExecutionException ex) {
-            ex.printStackTrace();
+            if (exitCode == 0) {
+                if (errors.isEmpty()) {
+                    LOG.info(String.format("%s exited without errors", dubCommand));
+                    //Messages.showInfoMessage(this.project, "Dub project imported", "Dub Import");
+                } else {
+                    LOG.warn(String.format("%s exited with %s errors", dubCommand, errors.size()));
+                    // potential error messages are things like:
+                    //   "No valid root package found - aborting."
+                    //   "Package vibe-d declared a sub-package, definition file is missing: /path/to/package"
+                    //   "Non-optional dependency vibe-d:core of vibe-d not found in dependency tree!?."
+                    // todo: do something useful with the errors
+                    Messages.showWarningDialog(this.project, String.format("%s exited with %s errors", dubCommand, errors.size()), "Dub Import");
+                }
+                final JsonObject jsonObject = new JsonParser()
+                    .parse(builder.toString())
+                    .getAsJsonObject();
+                return Optional.of(jsonObject);
+            } else {
+                LOG.error(String.format("%s exited with %s", dubCommand, exitCode));
+                // todo: do something useful with the errors
+                Messages.showErrorDialog(this.project, String.format("%s exited with %s", dubCommand, exitCode), "Dub Import");
+            }
+        } catch (ExecutionException | JsonSyntaxException e) {
+            LOG.error("Unable to parse dub configuration", e);
+            e.printStackTrace();
         }
-        return null;
+        return Optional.empty();
     }
 
 
---
+++ src/test/java/net/masterthought/dlanguage/project/DubConfigurationParserTest.java
@@ -0,0 +1,70 @@
+package net.masterthought.dlanguage.project;
+
+import com.intellij.testFramework.LightPlatformTestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+/**
+ * @author singingbush
+ */
+public class DubConfigurationParserTest extends LightPlatformTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // I'm using the dub.sdl from Vibe-d as test data as it's a big multi module project
+        loadTestFile("dub/vibe-d/dub.sdl", "dub.sdl");
+        loadTestFile("dub/vibe-d/core/dub.sdl", "core/dub.sdl");
+        loadTestFile("dub/vibe-d/crypto/dub.sdl", "crypto/dub.sdl");
+        loadTestFile("dub/vibe-d/data/dub.sdl", "data/dub.sdl");
+        loadTestFile("dub/vibe-d/diet/dub.sdl", "diet/dub.sdl");
+        loadTestFile("dub/vibe-d/http/dub.sdl", "http/dub.sdl");
+        loadTestFile("dub/vibe-d/inet/dub.sdl", "inet/dub.sdl");
+        loadTestFile("dub/vibe-d/mail/dub.sdl", "mail/dub.sdl");
+        loadTestFile("dub/vibe-d/mongodb/dub.sdl", "mongodb/dub.sdl");
+        loadTestFile("dub/vibe-d/redis/dub.sdl", "redis/dub.sdl");
+        loadTestFile("dub/vibe-d/stream/dub.sdl", "stream/dub.sdl");
+        loadTestFile("dub/vibe-d/textfilter/dub.sdl", "textfilter/dub.sdl");
+        loadTestFile("dub/vibe-d/utils/dub.sdl", "utils/dub.sdl");
+        loadTestFile("dub/vibe-d/web/dub.sdl", "web/dub.sdl");
+    }
+
+    public void testDubParser() throws Exception {
+        final DubConfigurationParser dubConfigurationParser = new DubConfigurationParser(ourProject, "dub");
+
+        final DubConfigurationParser.DubPackage rootPackage = dubConfigurationParser.getDubPackage();
+        assertNotNull("The root package should have been parsed", rootPackage);
+
+        final List<DubConfigurationParser.DubPackage> subPackages = dubConfigurationParser.getDubPackageDependencies();
+        assertNotEmpty(subPackages);
+        assertEquals("there should be 21 dependencies", 21, subPackages.size());
+    }
+
+    private void loadTestFile(final String resourceLocation, final String destination) throws URISyntaxException, IOException {
+        final URL testResource = this.getClass().getClassLoader().getResource(resourceLocation);
+        final Path resourcePath = Paths.get(testResource.toURI());
+        final String baseDir = ourProject.getBaseDir().getCanonicalPath();
+        if(destination.contains("/")) {
+            final String[] path = destination.split("/");
+            final File subFolder = new File(baseDir, path[0]);
+            subFolder.mkdir();
+            Files.copy(resourcePath, Paths.get(baseDir, path[0], path[1]), StandardCopyOption.REPLACE_EXISTING);
+            new File(subFolder.getCanonicalPath(), "source").mkdir(); // they need source folder
+            new File(subFolder.getCanonicalPath(), "source/dummy.d").createNewFile(); // with at least 1 D file
+        } else {
+            Files.copy(resourcePath, Paths.get(baseDir, destination), StandardCopyOption.REPLACE_EXISTING);
+            new File(baseDir, "source").mkdir(); // needs source folder
+            new File(baseDir, "lib").mkdir(); // also the root dub file references a lib directory
+        }
+    }
+
+}
