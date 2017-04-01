19ca283b2b0635a54718dc3d1431360c1bf5e6ae
kingsleyh <kingsley.xa.hendrickse@barclayscapital.com>
2017-04-01 21:09:38 +0100

    kings - initial work on code formatter

---
+++ src/main/java/net/masterthought/dlanguage/features/formatter/settings/DLanguageCodeStyleSettingsProvider.java
@@ -0,0 +1,52 @@
+package net.masterthought.dlanguage.features.formatter.settings;
+
+import com.intellij.application.options.IndentOptionsEditor;
+import com.intellij.application.options.SmartIndentOptionsEditor;
+import com.intellij.lang.Language;
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
+import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider;
+import net.masterthought.dlanguage.DLanguage;
+import org.jetbrains.annotations.NotNull;
+
+public class DLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSettingsProvider {
+
+    private static final String DEFAULT_CODE_SAMPLE =
+        "module example;\n" +
+            "\n" +
+            "import std.stdio;\n" +
+            "\n" +
+            "void main() {\n" +
+            "\twriteln(\"Hello\");\n" +
+            "}";
+
+    @NotNull
+    @Override
+    public Language getLanguage() {
+        return DLanguage.INSTANCE;
+    }
+
+    @NotNull
+    @Override
+    public String getCodeSample(@NotNull SettingsType settingsType) {
+        return DEFAULT_CODE_SAMPLE;
+    }
+
+    @Override
+    public IndentOptionsEditor getIndentOptionsEditor() {
+        return new SmartIndentOptionsEditor();
+    }
+
+    @Override
+    public CommonCodeStyleSettings getDefaultCommonSettings() {
+        CommonCodeStyleSettings defaultSettings = new CommonCodeStyleSettings(getLanguage());
+        CommonCodeStyleSettings.IndentOptions indentOptions = defaultSettings.initIndentOptions();
+        indentOptions.INDENT_SIZE = 8;
+        indentOptions.CONTINUATION_INDENT_SIZE = 8;
+        indentOptions.TAB_SIZE = 8;
+        indentOptions.USE_TAB_CHARACTER = true;
+
+        defaultSettings.BLOCK_COMMENT_AT_FIRST_COLUMN = false;
+        defaultSettings.LINE_COMMENT_AT_FIRST_COLUMN = false;
+        return defaultSettings;
+    }
+}
