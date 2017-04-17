e59fbf60e0299b4b0cf15e13694fe847d2f18a67
singingbush <singingbush@hotmail.com>
2017-04-17 10:16:06 +0100

    issue #86 DubProjectImportProvider should handle dub.sdl

--- src/main/java/net/masterthought/dlanguage/project/DubProjectImportProvider.java
+++ src/main/java/net/masterthought/dlanguage/project/DubProjectImportProvider.java
@@ -1,18 +1,18 @@
 package net.masterthought.dlanguage.project;
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.ProjectJdkForModuleStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
-import net.masterthought.dlanguage.DLanguageSdkType;
 import net.masterthought.dlanguage.module.DubBinaryForModuleStep;
-import net.masterthought.dlanguage.module.DubInitForModuleStep;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 public class DubProjectImportProvider extends ProjectImportProvider {
@@ -32,22 +32,35 @@ public class DubProjectImportProvider extends ProjectImportProvider {
     }
 
     @Override
-    public boolean canImport(VirtualFile fileOrDirectory, @Nullable Project project) {
+    public boolean canImport(@NotNull final VirtualFile fileOrDirectory, @Nullable Project project) {
         // If we're not importing a directory, validate it as a file.
         if (!fileOrDirectory.isDirectory()) return canImportFromFile(fileOrDirectory);
 
-        VirtualFile dubFile = fileOrDirectory.findChild("dub.json");
-        if(dubFile != null){
-            if(canImportFromFile(dubFile)) return true;
-        }
-        VirtualFile[] dubFiles = fileOrDirectory.getChildren();
-        List<Boolean> vfiles = new ArrayList<>();
-        for(VirtualFile vfile : dubFiles){
-            if(!vfile.isDirectory() && vfile.getName().equals("dub.json")) {
-                vfiles.add(canImportFromFile(vfile));
+        // check for dub.json
+        final VirtualFile dubJson = fileOrDirectory.findChild("dub.json");
+        if(dubJson != null) {
+            if(canImportFromFile(dubJson)) {
+                return true;
             }
         }
-        return !vfiles.contains(false);
+
+        // check for dub.sdl
+        final VirtualFile dubSdl = fileOrDirectory.findChild("dub.sdl");
+        if(dubSdl != null) {
+            if(canImportFromFile(dubSdl)) {
+                return true;
+            }
+        }
+
+        // alternatively, check all the children for a dub.json or a dub.sdl
+        final VirtualFile[] dubFiles = fileOrDirectory.getChildren();
+
+        final Optional<VirtualFile> optionalDubFile = Arrays.stream(dubFiles)
+            .filter(f -> !f.isDirectory())
+            .filter(f -> f.getNameWithoutExtension().equalsIgnoreCase("dub"))
+            .findFirst();
+
+        return optionalDubFile.isPresent() && canImportFromFile(optionalDubFile.get());
     }
 
     @Override
