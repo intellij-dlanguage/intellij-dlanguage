8cff1e40919c083bf497aebf074f491cdd5d6820
Francis Nixon <13nixonf@gmail.com>
2017-11-03 21:18:23 +0000

    fixed error with unions not being indexed and shortened some long full name usages in kotlin

--- src/main/kotlin/io/github/intellij/dlanguage/stubs/DlangUnionDeclarationStub.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/stubs/DlangUnionDeclarationStub.kt
@@ -6,7 +6,7 @@ import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
 import io.github.intellij.dlanguage.psi.DlangUnionDeclaration
 
-class DlangUnionDeclarationStub : NamedStubBase<io.github.intellij.dlanguage.psi.DlangUnionDeclaration> {
+class DlangUnionDeclarationStub : NamedStubBase<DlangUnionDeclaration> {
     constructor(parent: StubElement<*>, elementType: IStubElementType<*, *>, name: StringRef?) : super(parent, elementType, name)
 
     constructor(parent: StubElement<*>, elementType: IStubElementType<*, *>, name: String?) : super(parent, elementType, name)
--- src/main/kotlin/io/github/intellij/dlanguage/stubs/index/DTopLevelDeclarationIndex.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/stubs/index/DTopLevelDeclarationIndex.kt
@@ -4,32 +4,33 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.*
 import io.github.intellij.dlanguage.index.DModuleIndex
+import io.github.intellij.dlanguage.index.DModuleIndex.getFilesByModuleName
 import io.github.intellij.dlanguage.psi.interfaces.DNamedElement
 import io.github.intellij.dlanguage.stubs.DlangIdentifierStub
 
-class DTopLevelDeclarationIndex : StringStubIndexExtension<io.github.intellij.dlanguage.psi.interfaces.DNamedElement>() {
+class DTopLevelDeclarationIndex : StringStubIndexExtension<DNamedElement>() {
 
     override fun getVersion(): Int {
         return super.getVersion() + VERSION
     }
 
-    override fun getKey(): StubIndexKey<String, io.github.intellij.dlanguage.psi.interfaces.DNamedElement> {
+    override fun getKey(): StubIndexKey<String, DNamedElement> {
         return KEY
     }
 
     companion object {
-        val KEY: StubIndexKey<String, io.github.intellij.dlanguage.psi.interfaces.DNamedElement> = StubIndexKey.createIndexKey<String, io.github.intellij.dlanguage.psi.interfaces.DNamedElement>("d.globally.accessible.name")
+        val KEY: StubIndexKey<String, DNamedElement> = StubIndexKey.createIndexKey<String, DNamedElement>("d.globally.accessible.name")
         val VERSION = 6
-        fun <S : NamedStubBase<T>, T : io.github.intellij.dlanguage.psi.interfaces.DNamedElement> indexTopLevelDeclarations(stub: S, sink: IndexSink, name: String) {
+        fun <S : NamedStubBase<T>, T : DNamedElement> indexTopLevelDeclarations(stub: S, sink: IndexSink, name: String) {
             if (stub !is io.github.intellij.dlanguage.stubs.DlangIdentifierStub && topLevelDeclaration<S, T>(stub)) {
                 sink.occurrence(DTopLevelDeclarationIndex.KEY, name)
             }
         }
 
-        fun getTopLevelSymbols(name: String, module: String, project: Project): MutableSet<io.github.intellij.dlanguage.psi.interfaces.DNamedElement> {
-            val elements = mutableSetOf<io.github.intellij.dlanguage.psi.interfaces.DNamedElement>()
-            for (file in io.github.intellij.dlanguage.index.DModuleIndex.getFilesByModuleName(project, module, GlobalSearchScope.everythingScope(project))) {
-                elements.addAll(StubIndex.getElements(KEY, name, project, GlobalSearchScope.fileScope(file), io.github.intellij.dlanguage.psi.interfaces.DNamedElement::class.java))
+        fun getTopLevelSymbols(name: String, module: String, project: Project): MutableSet<DNamedElement> {
+            val elements = mutableSetOf<DNamedElement>()
+            for (file in getFilesByModuleName(project, module, GlobalSearchScope.everythingScope(project))) {
+                elements.addAll(StubIndex.getElements(KEY, name, project, GlobalSearchScope.fileScope(file), DNamedElement::class.java))
             }
             return elements
         }
