d4dcfe54732a68b248e0e3e7ffc68d959b86fa10
Francis Nixon <13nixonf@gmail.com>
2017-11-16 11:11:16 +0000

    record plugin mme and version as well as don't record null versions

--- src/main/java/de/halirutan/mathematica/errorreporting/IdeaInformationProxy.java
+++ src/main/java/de/halirutan/mathematica/errorreporting/IdeaInformationProxy.java
@@ -21,17 +21,14 @@
 
 package de.halirutan.mathematica.errorreporting;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.application.ex.ApplicationInfoEx;
 import com.intellij.openapi.diagnostic.Attachment;
 import com.intellij.openapi.extensions.PluginDescriptor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectCoreUtil;
-import com.intellij.projectImport.ProjectImportBuilder;
-import com.intellij.psi.PsiManager;
 import com.intellij.util.SystemProperties;
-
 import io.github.intellij.dlanguage.settings.DLanguageToolsConfigurable;
 import io.github.intellij.dlanguage.settings.ToolKey;
 import java.util.LinkedHashMap;
@@ -48,7 +45,6 @@ public class IdeaInformationProxy {
         final PluginDescriptor pluginDescriptor) {
         final LinkedHashMap<String, String> params = new LinkedHashMap<>(30);
 
-
         params.put("error.description", error.getDescription());
 
         params.put("Plugin Name", error.getPluginName());
@@ -56,46 +52,69 @@ public class IdeaInformationProxy {
 
         params.put("Plugin Id", pluginDescriptor.getPluginId().getIdString());
 
+        final IdeaPluginDescriptor plugin = PluginManager.getPlugin(pluginDescriptor.getPluginId());
+        if (plugin != null) {
+            params.put("Plugin Name FallBack", plugin.getName());
+            params.put("Plugin Version Fallback", plugin.getVersion());
+        }
+
         params.put("OS Name", SystemProperties.getOsName());
         params.put("Java version", SystemProperties.getJavaVersion());
         params.put("Java vm vendor", SystemProperties.getJavaVmVendor());
 
         params.put("App Name", namesInfo.getProductName());
         params.put("App Full Name", namesInfo.getFullProductName());
-        params.put("App Version name", appInfo.getVersionName());
         params.put("Is EAP", Boolean.toString(appInfo.isEAP()));
         params.put("App Build", appInfo.getBuild().asString());
         params.put("App Version", appInfo.getFullVersion());
-
         params.put("Last Action", error.getLastAction());
 
-        final String dubVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DUB_KEY.getPath(), "--version");
-        params.put("Dub Version", dubVersion);
+        final String dubPath = ToolKey.DUB_KEY.getPath();
+        if (dubPath != null) {
+            final String dubVersion = DLanguageToolsConfigurable
+                .getVersion(dubPath, "--version");
+            params.put("Dub Version", dubVersion);
+        }
 
-        final String dcdServerVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DCD_SERVER_KEY.getPath(), "--version");
-        params.put("DCD Server Version", dcdServerVersion);
+        final String dcdServerPath = ToolKey.DCD_SERVER_KEY.getPath();
+        if (dcdServerPath != null) {
+            final String dcdServerVersion = DLanguageToolsConfigurable
+                .getVersion(dcdServerPath, "--version");
+            params.put("DCD Server Version", dcdServerVersion);
+        }
 
-        final String dcdClientVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DCD_CLIENT_KEY.getPath(), "--version");
-        params.put("DCD Client Version", dcdClientVersion);
+        final String dcdClientPath = ToolKey.DCD_CLIENT_KEY.getPath();
+        if (dcdClientPath != null) {
+            final String dcdClientVersion = DLanguageToolsConfigurable
+                .getVersion(dcdClientPath, "--version");
+            params.put("DCD Client Version", dcdClientVersion);
+        }
 
-        final String DScannerVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DSCANNER_KEY.getPath(), "--version");
-        params.put("DScanner Version", DScannerVersion);
+        final String DScannerPath = ToolKey.DSCANNER_KEY.getPath();
+        if (DScannerPath != null) {
+            final String DScannerVersion = DLanguageToolsConfigurable
+                .getVersion(DScannerPath, "--version");
+            params.put("DScanner Version", DScannerVersion);
+        }
 
-        final String GDBVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.GDB_KEY.getPath(), "--version");
-        params.put("GDB Version", GDBVersion);
+        final String gdbPath = ToolKey.GDB_KEY.getPath();
+        if (gdbPath != null) {
+            final String GDBVersion = DLanguageToolsConfigurable
+                .getVersion(gdbPath, "--version");
+            params.put("GDB Version", GDBVersion);
+        }
 
-        final String DFormatVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DFORMAT_KEY.getPath(), "--version");
-        params.put("D Format Version", DFormatVersion);
+        if (ToolKey.DFORMAT_KEY.getPath() != null) {
+            final String DFormatVersion = DLanguageToolsConfigurable
+                .getVersion(ToolKey.DFORMAT_KEY.getPath(), "--version");
+            params.put("D Format Version", DFormatVersion);
+        }
 
-        final String DFixVersion = DLanguageToolsConfigurable
-            .getVersion(ToolKey.DFIX_KEY.getPath(), "--version");
-        params.put("DFix Version", DFixVersion);
+        if (ToolKey.DFIX_KEY.getPath() != null) {
+            final String DFixVersion = DLanguageToolsConfigurable
+                .getVersion(ToolKey.DFIX_KEY.getPath(), "--version");
+            params.put("DFix Version", DFixVersion);
+        }
 
         params.put("error.message", error.getMessage());
         params.put("error.stacktrace", error.getStackTrace());
