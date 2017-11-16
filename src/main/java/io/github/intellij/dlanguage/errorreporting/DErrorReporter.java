32e4f946421e6db292aeb126e663c8b9538d8434
Francis Nixon <13nixonf@gmail.com>
2017-11-16 10:27:36 +0000

    removed bad import

--- src/main/java/io/github/intellij/dlanguage/errorreporting/DErrorReporter.java
+++ src/main/java/io/github/intellij/dlanguage/errorreporting/DErrorReporter.java
@@ -14,7 +14,6 @@ import com.intellij.util.Consumer;
 import de.halirutan.mathematica.errorreporting.GitHubErrorBean;
 import de.halirutan.mathematica.errorreporting.IdeaInformationProxy;
 import io.sentry.Sentry;
-import org.eclipse.core.internal.resources.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
