513474430fd631ca87406d64d5584ea16f58925d
SingingBush <singingbush@hotmail.com>
2017-11-20 23:41:51 +0000

    put projectOpenProcessor back in

--- src/main/java/io/github/intellij/dlanguage/project/DubProjectOpenProcessor.java
+++ src/main/java/io/github/intellij/dlanguage/project/DubProjectOpenProcessor.java
@@ -1,38 +1,56 @@
-//package io.github.intellij.dlanguage.project;
-//
-//import com.intellij.ide.util.projectWizard.WizardContext;
-//import com.intellij.openapi.options.ConfigurationException;
-//import com.intellij.openapi.vfs.VirtualFile;
-//import com.intellij.projectImport.ProjectOpenProcessorBase;
-//import org.jetbrains.annotations.NotNull;
-//import org.jetbrains.annotations.Nullable;
-//
-//import java.util.List;
-//
-//
-//public class DubProjectOpenProcessor extends ProjectOpenProcessorBase<DubProjectImportBuilder> {
-//    public DubProjectOpenProcessor(@NotNull final DubProjectImportBuilder builder) {
-//        super(builder);
-//    }
-//
-//    @Nullable
-//    public String[] getSupportedExtensions() {
-//        return new String[] {"dub.json"};
-//    }
-//
-//    public boolean doQuickImport(VirtualFile file, final WizardContext wizardContext) {
-//        getBuilder().setRootDirectory(file.getParent().getPath());
-//
-//        final List<String> projects = getBuilder().getList();
-//        if (projects == null || projects.size() != 1) {
-//            return false;
-//        }
-//        try {
-//            getBuilder().setList(projects);
-//        } catch (ConfigurationException e) {
-//            e.printStackTrace();
-//        }
-//        wizardContext.setProjectName("woops");
-//        return true;
-//    }
-//}
+package io.github.intellij.dlanguage.project;
+
+import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.projectImport.ProjectOpenProcessorBase;
+import io.github.intellij.dlanguage.DlangBundle;
+import io.github.intellij.dlanguage.icons.DlangIcons;
+import io.github.intellij.dlanguage.settings.ToolKey;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Used when opening a dub project within the IDE
+ */
+public class DubProjectOpenProcessor extends ProjectOpenProcessorBase<DubProjectImportBuilder> {
+
+    private static final Logger LOG = Logger.getInstance(DubProjectOpenProcessor.class.getName());
+
+    public DubProjectOpenProcessor(@NotNull final DubProjectImportBuilder builder) {
+        super(builder);
+    }
+
+    @Nullable
+    public String[] getSupportedExtensions() {
+        return new String[] {"dub.json","dub.sdl"};
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return DlangIcons.FILE;
+    }
+
+    public boolean doQuickImport(final VirtualFile file, final WizardContext context) {
+        final DubProjectImportBuilder builder = getBuilder();
+        final VirtualFile rootDirectory = file.getParent();
+
+        if(StringUtil.isNotEmpty(ToolKey.DUB_KEY.getPath())) {
+            builder.getParameters().dubBinary = ToolKey.DUB_KEY.getPath();
+            //builder.setRootDirectory(context.getProjectFileDirectory());
+            builder.setRootDirectory(rootDirectory.getPath());
+            context.setProjectName(rootDirectory.getName());
+            LOG.debug("Opening dub project");
+            return true;
+        } else {
+            LOG.warn("Couldn't open project as dub not configured");
+            Messages.showInfoMessage(DlangBundle.INSTANCE.message("d.ui.projectopen.missing.dub.binary"), DlangBundle.INSTANCE.message("d.ui.projectopen.dub"));
+            return false;
+        }
+    }
+}
