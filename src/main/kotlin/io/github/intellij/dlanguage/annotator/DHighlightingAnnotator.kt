16d82fd3f6ef8cef6a05f59081ba90f215aeecb3
Andrey Kabylin <andrey@kabylin.ru>
2017-11-25 22:52:00 +0700

    refactoring highlighter

---
+++ src/main/kotlin/io/github/intellij/dlanguage/annotator/DHighlightingAnnotator.kt
@@ -0,0 +1,11 @@
+package io.github.intellij.dlanguage.annotator
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.psi.PsiElement
+
+class DHighlightingAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+
+    }
+}
