b6f37e3882d71631499fb1d63ada723425c57ba3
Francis Nixon <13nixonf@gmail.com>
2017-11-25 23:58:44 +0000

    added empty statement inspection/quickfix

---
+++ src/main/kotlin/io/github/intellij/dlanguage/inspections/EmptyStatement.kt
@@ -0,0 +1,29 @@
+package io.github.intellij.dlanguage.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import io.github.intellij.dlanguage.DlangBundle
+import io.github.intellij.dlanguage.psi.DlangVisitor
+import io.github.intellij.dlanguage.psi.impl.DLanguageDeclarationImpl
+import io.github.intellij.dlanguage.quickfix.RemoveEmptyStatement
+
+/**
+ * Created by francis on 11/25/2017.
+ */
+class EmptyStatement : LocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return EmptyStatementVisitor(holder)
+    }
+
+    override fun getDescriptionFileName(): String = "RenameStatement.html"
+    override fun getDisplayName(): String = "Empty Statement"
+    override fun getGroupDisplayName(): String = DlangBundle.message("d.inspections.groupname")
+}
+
+class EmptyStatementVisitor(val holder: ProblemsHolder) : DlangVisitor() {
+    override fun visitDeclaration(o: DLanguageDeclarationImpl) {
+        if (o.text == ";")
+            holder.registerProblem(o, "Empty Statement", RemoveEmptyStatement(o))
+    }
+}
---
+++ src/main/kotlin/io/github/intellij/dlanguage/quickfix/RemoveEmptyStatement.kt
@@ -0,0 +1,26 @@
+package io.github.intellij.dlanguage.quickfix
+
+import com.intellij.codeInspection.LocalQuickFixOnPsiElement
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import io.github.intellij.dlanguage.psi.DLanguageDeclaration
+
+/**
+ * Created by francis on 11/25/2017.
+ */
+class RemoveEmptyStatement(elem: DLanguageDeclaration) : LocalQuickFixOnPsiElement(elem) {
+    /**
+     * @return text to appear in "Apply Fix" popup when multiple Quick Fixes exist (in the results of batch code inspection). For example,
+     * if the name of the quickfix is "Create template &lt;filename&gt", the return value of getFamilyName() should be "Create template".
+     * If the name of the quickfix does not depend on a specific element, simply return getName().
+     */
+    override fun getFamilyName(): String = "DLang"//todo internationalize/better name
+
+    override fun getText(): String = "Remove Semicolon"
+
+    override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
+        getStartElement().delete()
+    }
+
+}
