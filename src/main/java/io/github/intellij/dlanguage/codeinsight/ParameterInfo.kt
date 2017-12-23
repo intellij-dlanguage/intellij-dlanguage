3fe40d5b01bfc1342b7c00bad8b19e843b1b39e8
Francis Nixon <13nixonf@gmail.com>
2017-12-22 16:18:11 -0800

    decent implementation of parameter info. Doesn't work for member functions.

--- src/main/java/io/github/intellij/dlanguage/codeinsight/ParameterInfo.kt
+++ src/main/java/io/github/intellij/dlanguage/codeinsight/ParameterInfo.kt
@@ -18,28 +18,31 @@ import io.github.intellij.dlanguage.resolve.DResolveUtil
 import io.github.intellij.dlanguage.utils.FunctionCallExpression
 import io.github.intellij.dlanguage.utils.Parameter
 
-class ParameterInfo : ParameterInfoHandler<FunctionCallExpression,Parameter> {
+class ParameterInfo : ParameterInfoHandler<FunctionCallExpression, Parameter> {
     override fun updateParameterInfo(parameterOwner: FunctionCallExpression, context: UpdateParameterInfoContext) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun getParametersForDocumentation(p: Parameter?, context: ParameterInfoContext?): Array<Any>? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("this doesn't need to be implemented b/c it is not used by intellij-core")
     }
 
     override fun tracksParameterIndex(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("this doesn't need to be implemented b/c it is not used by intellij-core")
     }
 
     override fun showParameterInfo(functionCallExpression: FunctionCallExpression, context: CreateParameterInfoContext) {
-        val reference = functionCallExpression.unaryExpression?.primaryExpression?.identifierOrTemplateInstance?.identifier?.reference
-        if(reference == null || reference !is DReference){
+        var reference = functionCallExpression.unaryExpression?.primaryExpression?.identifierOrTemplateInstance?.identifier?.reference
+        if (reference == null) {
+            reference = functionCallExpression.unaryExpression?.primaryExpression?.identifierOrTemplateInstance?.templateInstance?.identifier?.reference
+        }
+        if (reference == null || reference !is DReference) {
             return
         }
         val definitionNodes = DResolveUtil.getInstance(functionCallExpression.project).findDefinitionNode(reference.element, false)
 //        val resolvedElements: Array<ResolveResult> = reference.multiResolve(false);
-        context.itemsToShow = definitionNodes.flatMap { findChildrenOfType(it,Parameter::class.java) }.filterNotNull().toTypedArray()
-        context.showHint(functionCallExpression,0,this)
+        context.itemsToShow = definitionNodes.flatMap { findChildrenOfType(it, Parameter::class.java) }.filterNotNull().toTypedArray()
+        context.showHint(functionCallExpression, 0, this)
     }
 
     override fun updateUI(p: Parameter, context: ParameterInfoUIContext) {
@@ -47,15 +50,20 @@ class ParameterInfo : ParameterInfoHandler<FunctionCallExpression,Parameter> {
     }
 
     override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): FunctionCallExpression? {
-        return null
+        val listStart = context.offset
+        val file = context.file
+        val element = file.findElementAt(listStart)
+        val functionCallExpression = getParentOfType(element, DLanguageFunctionCallExpression::class.java)
+        return functionCallExpression;
     }
 
     override fun getParameterCloseChars(): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("this doesn't need to be implemented b/c it is not used by intellij-core")
     }
 
-    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<Any>? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getParametersForLookup(item: LookupElement, context: ParameterInfoContext): Array<Any>? {
+        //todo I'm not sure what this is meant to do.
+        return arrayOf(item)
     }
 
     override fun couldShowInLookup(): Boolean {
