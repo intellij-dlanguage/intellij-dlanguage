6d357582c112ab61ee82d042174a5cf73c0928a1
Francis Nixon <13nixonf@gmail.com>
2017-12-24 09:53:38 -0800

    fixed tests and added issue77 testing

--- src/test/java/io/github/intellij/dlanguage/highlighting/DlangHighlightingLexerTest.java
+++ src/test/java/io/github/intellij/dlanguage/highlighting/DlangHighlightingLexerTest.java
@@ -188,4 +188,5 @@ public class DlangHighlightingLexerTest extends DHighlightingLexerTestBase {
     public void testtokens_unordered_le() { doTest(true, true); }
     public void testtokens_xor() { doTest(true, true); }
     public void testtokens_xor_assign() { doTest(true, true); }
+    public void testissue77() {doTest(true,true);}
 }
