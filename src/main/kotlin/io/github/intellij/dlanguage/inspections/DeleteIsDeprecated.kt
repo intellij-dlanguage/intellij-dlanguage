9a7d2351e68014cd3cb2fda1a05a10f09299ce3d
Francis Nixon <13nixonf@gmail.com>
2018-01-05 18:12:53 -0800

    DeleteIsDeprecated implmented

---
+++ src/main/kotlin/io/github/intellij/dlanguage/inspections/DeleteIsDeprecated.kt
@@ -0,0 +1,27 @@
+package io.github.intellij.dlanguage.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import io.github.intellij.dlanguage.DlangBundle
+import io.github.intellij.dlanguage.psi.DlangVisitor
+import io.github.intellij.dlanguage.psi.impl.DLanguageDeleteExpressionImpl
+
+/**
+ * Created by francis on 1/5/2018.
+ */
+class DeleteIsDeprecated : LocalInspectionTool() {
+    override fun getDescriptionFileName(): String = "DeleteIsDeprecated.html"
+    override fun getDisplayName(): String = "DeleteIsDeprecated"
+    override fun getGroupDisplayName(): String = DlangBundle.message("d.inspections.groupname")
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return DeleteIsDeprecatedVisitor(holder)
+    }
+}
+
+class DeleteIsDeprecatedVisitor(val holder: ProblemsHolder) : DlangVisitor() {
+    override fun visitDeleteExpression(o: DLanguageDeleteExpressionImpl) {
+        holder.registerProblem(o, "Avoid using the 'delete' keyword. It is deprecated")
+    }
+}
