7ce960e7328175f797edb578ed7e5a0b1088fe5f
Francis Nixon <13nixonf@gmail.com>
2018-01-17 18:12:42 +0000

    fix #340 by registering version related Undefined symbol warnings as weak warnings.

--- src/main/kotlin/io/github/intellij/dlanguage/inspections/PossiblyUndefinedSymbol.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/inspections/PossiblyUndefinedSymbol.kt
@@ -16,8 +16,10 @@ import io.github.intellij.dlanguage.DlangBundle
 import io.github.intellij.dlanguage.psi.DlangVisitor
 import io.github.intellij.dlanguage.psi.impl.named.DlangIdentifierImpl
 import io.github.intellij.dlanguage.resolve.DResolveUtil
+import io.github.intellij.dlanguage.resolve.SpecialCaseResolve
 import io.github.intellij.dlanguage.utils.Identifier
 import io.github.intellij.dlanguage.utils.ModuleDeclaration
+import io.github.intellij.dlanguage.utils.VersionCondition
 
 
 /**
@@ -34,8 +36,12 @@ fun symbolIsDefinedByDefault(identifier: Identifier): Boolean {
 
 class PossiblyUndefinedSymbol : LocalInspectionTool() {
     class UndefinedSymbolVisitor(val holder: ProblemsHolder) : DlangVisitor() {
-
+        val objectDotDContents = setOf<String>("string", "size_t", "ptrdiff_t", "_d_newclass", "rt_finalize", "object", "sizediff_t", "hash_t", "equals_t", "wstring", "dstring", "selector", "Object", "toString", "toHash", "opCmp", "opEquals", "Monitor", "lock", "unlock", "factory", "opEquals", "_d_setSameMutex", "setSameMutex", "Interface", "classinfo", "vtbl", "offset", "OffsetTypeInfo", "ti", "TypeInfo", "getHash", "equals", "compare", "tsize", "swap", "next", "initializer", "init", "flags", "offTi", "destroy", "postblit", "talign", "argTypes", "rtInfo", "TypeInfo_Typedef", "base", "name", "m_init", "TypeInfo_Enum", "TypeInfo_Pointer", "m_next", "TypeInfo_Array", "TypeInfo_StaticArray", "value", "len", "TypeInfo_AssociativeArray", "value", "key", "TypeInfo_Vector", "TypeInfo_Function", "deco", "TypeInfo_Delegate", "TypeInfo_Class", "interfaces", "ClassFlags", "classInvariant", "m_flags", "deallocator", "m_offTi", "defaultConstructor", "m_RTInfo", "find", "create", "ClassInfo", "TypeInfo_Interface", "TypeInfo_Struct", "xtoHash", "xopEquals", "xopCmp", "xtoString", "StructFlags", "hasPointers", "isDynamicType", "m_flags", "xdtor", "xdtorti", "xpostblit", "m_align", "TypeInfo_Tuple", "elements", "TypeInfo_Invariant", "TypeInfo_Const", "TypeInfo_Shared", "TypeInfo_Inout", "MIctorstart", "MIctordone", "MIstandalone", "MItlsctor", "MItlsdtor", "MIctor", "MIdtor", "MIxgetMembers", "MIictor", "MIunitTest", "MIimportedModules", "MIlocalClasses", "MIname", "ModuleInfo", "_flags", "_index", "opAssign", "ctor", "dtor", "ictor", "Throwable", "TraceInfo", "Exception", "Error")//todo this isn't all public symbols in object.d
         val log: Logger = Logger.getInstance(this::class.java)
+
+        fun isVersion(identifier: DlangIdentifierImpl) = identifier.parent is VersionCondition
+
+
         override fun visitIdentifier(identifier: DlangIdentifierImpl) {
             if (identifier != null) {
                 val start = System.currentTimeMillis()
@@ -51,9 +57,11 @@ class PossiblyUndefinedSymbol : LocalInspectionTool() {
 //                    holder.registerProblem(identifier, "Possibly undefined symbol")
 //                }
                 if (DResolveUtil.getInstance(identifier.project).findDefinitionNode(identifier, false).isEmpty() && !symbolIsDefinedByDefault(identifier)) {
-                    val objectDotDContents = setOf<String>("string","size_t","ptrdiff_t","_d_newclass","rt_finalize","object","sizediff_t","hash_t","equals_t","wstring","dstring","selector","Object","toString","toHash","opCmp","opEquals","Monitor","lock","unlock","factory","opEquals","_d_setSameMutex","setSameMutex","Interface","classinfo","vtbl","offset","OffsetTypeInfo","ti","TypeInfo","getHash","equals","compare","tsize","swap","next","initializer","init","flags","offTi","destroy","postblit","talign","argTypes","rtInfo","TypeInfo_Typedef","base","name","m_init","TypeInfo_Enum","TypeInfo_Pointer","m_next","TypeInfo_Array","TypeInfo_StaticArray","value","len","TypeInfo_AssociativeArray","value","key","TypeInfo_Vector","TypeInfo_Function","deco","TypeInfo_Delegate","TypeInfo_Class","interfaces","ClassFlags","classInvariant","m_flags","deallocator","m_offTi","defaultConstructor","m_RTInfo","find","create","ClassInfo","TypeInfo_Interface","TypeInfo_Struct","xtoHash","xopEquals","xopCmp","xtoString","StructFlags","hasPointers","isDynamicType","m_flags","xdtor","xdtorti","xpostblit","m_align","TypeInfo_Tuple","elements","TypeInfo_Invariant","TypeInfo_Const","TypeInfo_Shared","TypeInfo_Inout","MIctorstart","MIctordone","MIstandalone","MItlsctor","MItlsdtor","MIctor","MIdtor","MIxgetMembers","MIictor","MIunitTest","MIimportedModules","MIlocalClasses","MIname","ModuleInfo","_flags","_index","opAssign","ctor","dtor","ictor","Throwable","TraceInfo","Exception","Error")//todo this isn't all public symbols in object.d
+
                     if (objectDotDContents.contains(identifier.name))
                         holder.registerProblem(identifier, "Possibly undefined symbol",SetupSDK(identifier.containingFile))
+                    else if (isVersion(identifier))
+                        holder.registerProblem(identifier, "Possibly undefined symbol", ProblemHighlightType.WEAK_WARNING)
                     else
                         holder.registerProblem(identifier, "Possibly undefined symbol")//todo add quick fix
                 }
@@ -76,7 +84,7 @@ class PossiblyUndefinedSymbol : LocalInspectionTool() {
 
 class SetupSDK(file: PsiFile) : LocalQuickFixOnPsiElement(file),HighPriorityAction{
     override fun getText(): String {
-        return "Setup SDK";//todo
+        return "Setup SDK"//todo
     }
 
     override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
