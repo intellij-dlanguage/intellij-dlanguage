ee41f12355c4a562538dc810727bf8f273128464
Francis Nixon <13nixonf@gmail.com>
2018-02-03 14:55:24 +0000

    fix  #357

--- src/main/java/io/github/intellij/dlanguage/psi/interfaces/HasMembers.java
+++ src/main/java/io/github/intellij/dlanguage/psi/interfaces/HasMembers.java
@@ -1,6 +1,7 @@
 package io.github.intellij.dlanguage.psi.interfaces;
 
 import com.intellij.extapi.psi.StubBasedPsiElementBase;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.psi.StubBasedPsiElement;
 import com.intellij.psi.stubs.NamedStubBase;
 import com.intellij.psi.stubs.Stub;
@@ -12,8 +13,10 @@ import io.github.intellij.dlanguage.psi.DLanguageSharedStaticDestructor;
 import io.github.intellij.dlanguage.stubs.*;
 import io.github.intellij.dlanguage.stubs.interfaces.DlangUnittestStub;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by francis on 8/5/2017.
@@ -22,11 +25,18 @@ public interface HasMembers<T extends StubElement> extends StubBasedPsiElement<T
     default Set<NamedStubBase> getMembers() {
         final Set<NamedStubBase> res = new HashSet<>();
         ((DlangFile) getContainingFile()).calcStubTree();
-        getMembersImpl(((StubBasedPsiElementBase) this).getGreenStub(), res);
+
+        final StubElement greenStub = ((StubBasedPsiElementBase) this).getGreenStub();
+        if (greenStub == null) {
+            Logger.getInstance(this.getClass())
+                .warn("Green stub was null for file:" + getContainingFile());
+            return Collections.emptySet();
+        }
+        getMembersImpl(greenStub, res);
         return res;
     }
 
-    default void getMembersImpl(final Stub stub, final Set<NamedStubBase> result) {
+    default void getMembersImpl(final @NotNull Stub stub, final Set<NamedStubBase> result) {
         for (final Stub childStub : stub.getChildrenStubs()) {
             if (childStub instanceof NamedStubBase && !(childStub instanceof DlangIdentifierStub)) {
                 result.add((NamedStubBase) childStub);
