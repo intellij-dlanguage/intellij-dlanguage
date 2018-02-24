1c7a378259c92d10233bbd9e45c9a384776210e2
Francis Nixon <13nixonf@gmail.com>
2018-02-24 13:50:59 +0000

    Updated dmd setup validator, to check sourcepaths/classpaths for required files.

--- src/main/kotlin/io/github/intellij/dlanguage/DLanguage.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/DLanguage.kt
@@ -7,19 +7,26 @@ import com.intellij.lang.ParserDefinition
 import com.intellij.lang.PsiParser
 import com.intellij.lexer.FlexAdapter
 import com.intellij.lexer.Lexer
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.roots.ui.configuration.ProjectSettingsService
+import com.intellij.openapi.util.Computable
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.TokenType
+import com.intellij.psi.search.GlobalSearchScope.*
 import com.intellij.psi.tree.TokenSet
 import com.intellij.psi.tree.TokenSet.create
 import io.github.intellij.dlanguage.dlanguage.DlangLexer
+import io.github.intellij.dlanguage.index.DModuleIndex.getFilesByModuleName
 import io.github.intellij.dlanguage.parser.ParserWrapper
 import io.github.intellij.dlanguage.psi.DlangFile
 import io.github.intellij.dlanguage.psi.DlangTypes
+import io.github.intellij.dlanguage.resolve.processors.basic.BasicResolve
 import io.github.intellij.dlanguage.stubs.types.DFileStubElementType
 import org.jetbrains.annotations.NotNull
 
@@ -30,16 +37,59 @@ class DLanguageLexerAdapter : FlexAdapter(DlangLexer())
 /**
  * This class is used to inform users that DMD is not setup for the project
  */
-class DLangProjectDmdSetupValidator: ProjectSdkSetupValidator {
+class DLangProjectDmdSetupValidator : ProjectSdkSetupValidator {
 
-    override fun isApplicableFor(project: Project, file: VirtualFile): Boolean =
-        DlangFileType.INSTANCE == file.fileType
+    val expectedModules = setOf("std.array", "std.ascii", "std.base64", "std.bigint", "std.bitmanip", "std.compiler", "std.complex", "std.concurrency", "std.conv", "std.csv", "std.demangle", "std.encoding", "std.exception", "std.file", "std.format", "std.functional", "std.getopt", "std.json", "std.math", "std.mathspecial", "std.meta", "std.mmfile", "std.numeric", "std.outbuffer", "std.parallelism", "std.path", "std.process", "std.random", "std.signals", "std.socket", "std.stdint", "std.stdio", "std.string", "std.system", "std.traits", "std.typecons", "std.uni", "std.uri", "std.utf", "std.uuid", "std.variant", "std.xml", "std.zip", "std.zlib")
+
+    override fun isApplicableFor(project: Project, file: VirtualFile): Boolean {
+        if (DlangFileType.INSTANCE != file.fileType) return false
+        val projectSdk = ProjectRootManager.getInstance(project).projectSdk
+        if (projectSdk == null)
+            return true
+        val sdkType = projectSdk.sdkType as? DlangSdkType ?: return true
+        if (!sdkType.isValidSdkHome(projectSdk.homePath)) {
+            return true
+        }
+        if (cannotFindObjectDotD(project)) {
+            return true
+        }
+        if (missingAnyPhobosFiles(project))
+            return true
+        return false
+    }
+
+    private fun missingAnyPhobosFiles(project: Project): Boolean =
+        !missingPhobosModules(project).isEmpty()
+
+    private fun missingPhobosModules(project: Project): List<String> {
+        val resolveFilesComputable = Computable({
+            expectedModules.filter {
+                getFilesByModuleName(project, it, allScope(project)).isEmpty()
+            }
+        }
+        )
+        return DumbService.getInstance(project).runReadActionInSmartMode(resolveFilesComputable)
+    }
+
+    private fun cannotFindObjectDotD(project: Project): Boolean {
+        val resolveObjectDotD: Computable<Boolean> = Computable({
+            BasicResolve.getInstance(project, profile = false).`object` == null
+        })
+        return DumbService.getInstance(project).runReadActionInSmartMode(resolveObjectDotD)
+    }
 
     override fun doFix(project: Project, file: VirtualFile) =
         Unit.apply { ProjectSettingsService.getInstance(project).openProjectSettings() }
 
-    override fun getErrorMessage(project: Project, file: VirtualFile): String? =
-        DlangBundle.message("d.ui.dmd.path.not.set")
+    override fun getErrorMessage(project: Project, file: VirtualFile): String? {
+        if (cannotFindObjectDotD(project)) {
+            return DlangBundle.message("d.ui.dmd.object.path.not.set")
+        }
+        if (missingAnyPhobosFiles(project)) {
+            return DlangBundle.message("d.ui.dmd.phobos.path.not.set") + missingPhobosModules(project).foldRight("", { s: String, acc: String -> acc + " " + s })
+        }
+        return DlangBundle.message("d.ui.dmd.path.not.set")
+    }
 }
 
 class DLangParserDefinition : ParserDefinition {
