51b2acdfd014b6de5c06c075d105efc63474133e
Jedzia <jed69@gmx.de>
2018-11-23 11:49:13 +0100

    now D is defined

---
+++ src/main/kotlin/io/github/intellij/dlanguage/template/DlangContext.kt
@@ -0,0 +1,11 @@
+package io.github.intellij.dlanguage.template
+
+import com.intellij.codeInsight.template.TemplateContextType
+import com.intellij.psi.PsiFile
+
+open class DlangContext protected constructor() : TemplateContextType("D", "D Language") {
+
+    override fun isInContext(file: PsiFile, offset: Int): Boolean {
+        return file.getName().endsWith(".d")
+    }
+}
