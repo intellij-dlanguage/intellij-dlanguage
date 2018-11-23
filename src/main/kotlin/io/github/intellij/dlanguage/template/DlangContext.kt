d0eab5387674a4bc5ba37ea45262281d45485098
Jedzia <jed69@gmx.de>
2018-11-23 13:43:35 +0100

    newer IDE's need a check for the correct language context.

--- src/main/kotlin/io/github/intellij/dlanguage/template/DlangContext.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/template/DlangContext.kt
@@ -6,6 +6,8 @@ import com.intellij.psi.PsiFile
 open class DlangContext protected constructor() : TemplateContextType("D", "D Language") {
 
     override fun isInContext(file: PsiFile, offset: Int): Boolean {
-        return file.name.endsWith(".d")
+        // Todo: in never IDE's (2018.2.x) the suffix check does not work. does the baseLanguage check superseeds this?
+        return (file.name.endsWith(".d")
+            || file.viewProvider.baseLanguage == io.github.intellij.dlanguage.DLanguage)
     }
 }
