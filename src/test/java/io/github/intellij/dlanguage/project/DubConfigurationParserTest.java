07afacdf2e3ea4c997653bc064d95fb024761f9b
singingbush <singingbush@hotmail.com>
2019-03-19 06:10:25 +0000

    #490 commented out DubConfigurationParserTest fully

--- src/test/java/io/github/intellij/dlanguage/project/DubConfigurationParserTest.java
+++ src/test/java/io/github/intellij/dlanguage/project/DubConfigurationParserTest.java
@@ -20,28 +20,29 @@ import java.util.stream.Stream;
 /**
  * @author singingbush
  */
-public class DubConfigurationParserTest extends LightPlatformTestCase {
+public class DubConfigurationParserTest { // extends LightPlatformTestCase {
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        final String baseDir = ourProject.getBaseDir().getCanonicalPath();
-        if(baseDir != null && !baseDir.isEmpty()) {
-            final File directory = new File(Paths.get(baseDir).toUri());
-            if(directory.isDirectory()) {
-                final File[] files = directory.listFiles();
-                if(files != null && files.length > 0) {
-                    //noinspection ResultOfMethodCallIgnored
-                    Stream.of(files).forEach(File::delete);
-                }
-            }
-        }
-        super.tearDown();
-    }
+//    todo: come back and rethink how we test dub
+//    @Override
+//    public void setUp() throws Exception {
+//        super.setUp();
+//    }
+//
+//    @Override
+//    public void tearDown() throws Exception {
+//        final String baseDir = ourProject.getBaseDir().getCanonicalPath();
+//        if(baseDir != null && !baseDir.isEmpty()) {
+//            final File directory = new File(Paths.get(baseDir).toUri());
+//            if(directory.isDirectory()) {
+//                final File[] files = directory.listFiles();
+//                if(files != null && files.length > 0) {
+//                    //noinspection ResultOfMethodCallIgnored
+//                    Stream.of(files).forEach(File::delete);
+//                }
+//            }
+//        }
+//        super.tearDown();
+//    }
 
 //    public void testDubParserWithVibed() throws Exception {
 //        loadVibedTestFiles();
@@ -96,40 +97,40 @@ public class DubConfigurationParserTest extends LightPlatformTestCase {
 //        assertEquals("The root dub.json for hibernated has 1 dependency", 1, rootPackage.getDependencies().size());
 //    }
 
-    private void loadVibedTestFiles() throws IOException, URISyntaxException {
-        // I'm using the dub.sdl from Vibe-d as test data as it's a big multi module project
-        loadTestFile("dub/vibe-d/dub.sdl", "dub.sdl");
-        loadTestFile("dub/vibe-d/core/dub.sdl", "core/dub.sdl");
-        loadTestFile("dub/vibe-d/crypto/dub.sdl", "crypto/dub.sdl");
-        loadTestFile("dub/vibe-d/data/dub.sdl", "data/dub.sdl");
-        loadTestFile("dub/vibe-d/diet/dub.sdl", "diet/dub.sdl");
-        loadTestFile("dub/vibe-d/http/dub.sdl", "http/dub.sdl");
-        loadTestFile("dub/vibe-d/inet/dub.sdl", "inet/dub.sdl");
-        loadTestFile("dub/vibe-d/mail/dub.sdl", "mail/dub.sdl");
-        loadTestFile("dub/vibe-d/mongodb/dub.sdl", "mongodb/dub.sdl");
-        loadTestFile("dub/vibe-d/redis/dub.sdl", "redis/dub.sdl");
-        loadTestFile("dub/vibe-d/stream/dub.sdl", "stream/dub.sdl");
-        loadTestFile("dub/vibe-d/textfilter/dub.sdl", "textfilter/dub.sdl");
-        loadTestFile("dub/vibe-d/utils/dub.sdl", "utils/dub.sdl");
-        loadTestFile("dub/vibe-d/web/dub.sdl", "web/dub.sdl");
-    }
-
-    private void loadTestFile(final String resourceLocation, final String destination) throws URISyntaxException, IOException {
-        final URL testResource = this.getClass().getClassLoader().getResource(resourceLocation);
-        final Path resourcePath = Paths.get(testResource.toURI());
-        final String baseDir = ourProject.getBaseDir().getCanonicalPath();
-        if (destination.contains("/")) {
-            final String[] path = destination.split("/");
-            final File subFolder = new File(baseDir, path[0]);
-            subFolder.mkdir();
-            Files.copy(resourcePath, Paths.get(baseDir, path[0], path[1]), StandardCopyOption.REPLACE_EXISTING);
-            new File(subFolder.getCanonicalPath(), "source").mkdir(); // they need source folder
-            new File(subFolder.getCanonicalPath(), "source/dummy.d").createNewFile(); // with at least 1 D file
-        } else {
-            Files.copy(resourcePath, Paths.get(baseDir, destination), StandardCopyOption.REPLACE_EXISTING);
-            new File(baseDir, "source").mkdir(); // needs source folder
-            new File(baseDir, "lib").mkdir(); // also the root dub file references a lib directory
-        }
-    }
+//    private void loadVibedTestFiles() throws IOException, URISyntaxException {
+//        // I'm using the dub.sdl from Vibe-d as test data as it's a big multi module project
+//        loadTestFile("dub/vibe-d/dub.sdl", "dub.sdl");
+//        loadTestFile("dub/vibe-d/core/dub.sdl", "core/dub.sdl");
+//        loadTestFile("dub/vibe-d/crypto/dub.sdl", "crypto/dub.sdl");
+//        loadTestFile("dub/vibe-d/data/dub.sdl", "data/dub.sdl");
+//        loadTestFile("dub/vibe-d/diet/dub.sdl", "diet/dub.sdl");
+//        loadTestFile("dub/vibe-d/http/dub.sdl", "http/dub.sdl");
+//        loadTestFile("dub/vibe-d/inet/dub.sdl", "inet/dub.sdl");
+//        loadTestFile("dub/vibe-d/mail/dub.sdl", "mail/dub.sdl");
+//        loadTestFile("dub/vibe-d/mongodb/dub.sdl", "mongodb/dub.sdl");
+//        loadTestFile("dub/vibe-d/redis/dub.sdl", "redis/dub.sdl");
+//        loadTestFile("dub/vibe-d/stream/dub.sdl", "stream/dub.sdl");
+//        loadTestFile("dub/vibe-d/textfilter/dub.sdl", "textfilter/dub.sdl");
+//        loadTestFile("dub/vibe-d/utils/dub.sdl", "utils/dub.sdl");
+//        loadTestFile("dub/vibe-d/web/dub.sdl", "web/dub.sdl");
+//    }
+//
+//    private void loadTestFile(final String resourceLocation, final String destination) throws URISyntaxException, IOException {
+//        final URL testResource = this.getClass().getClassLoader().getResource(resourceLocation);
+//        final Path resourcePath = Paths.get(testResource.toURI());
+//        final String baseDir = ourProject.getBaseDir().getCanonicalPath();
+//        if (destination.contains("/")) {
+//            final String[] path = destination.split("/");
+//            final File subFolder = new File(baseDir, path[0]);
+//            subFolder.mkdir();
+//            Files.copy(resourcePath, Paths.get(baseDir, path[0], path[1]), StandardCopyOption.REPLACE_EXISTING);
+//            new File(subFolder.getCanonicalPath(), "source").mkdir(); // they need source folder
+//            new File(subFolder.getCanonicalPath(), "source/dummy.d").createNewFile(); // with at least 1 D file
+//        } else {
+//            Files.copy(resourcePath, Paths.get(baseDir, destination), StandardCopyOption.REPLACE_EXISTING);
+//            new File(baseDir, "source").mkdir(); // needs source folder
+//            new File(baseDir, "lib").mkdir(); // also the root dub file references a lib directory
+//        }
+//    }
 
 }
