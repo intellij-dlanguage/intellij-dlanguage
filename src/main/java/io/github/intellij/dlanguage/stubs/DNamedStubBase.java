c851556a7c8acf61d0eae85b322dbca618b18724
SingingBush <singingbush@hotmail.com>
2020-10-11 13:54:39 +0100

    #443 clearer logging of missing importedModule in constructor

--- src/main/java/io/github/intellij/dlanguage/stubs/DNamedStubBase.java
+++ src/main/java/io/github/intellij/dlanguage/stubs/DNamedStubBase.java
@@ -7,6 +7,7 @@ import com.intellij.util.io.StringRef;
 import io.github.intellij.dlanguage.psi.interfaces.DNamedElement;
 import io.github.intellij.dlanguage.resolve.processors.parameters.DAttributes;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by francis on 12/3/2017.
@@ -16,16 +17,17 @@ public class DNamedStubBase<T extends DNamedElement> extends NamedStubBase<T> {
     private final DAttributes attributes;
 
     protected DNamedStubBase(final StubElement parent,
-        @NotNull final IStubElementType elementType,
-        final StringRef name,
-        final DAttributes attributes) {
+                             @NotNull final IStubElementType elementType,
+                             @Nullable final StringRef name,
+                             final DAttributes attributes) {
         super(parent, elementType, name);
         this.attributes = attributes;
     }
 
     protected DNamedStubBase(final StubElement parent,
-        @NotNull final IStubElementType elementType, final String name,
-        final DAttributes attributes) {
+                             @NotNull final IStubElementType elementType,
+                             @Nullable final String name,
+                             final DAttributes attributes) {
         super(parent, elementType, name);
         this.attributes = attributes;
     }
--- src/main/java/io/github/intellij/dlanguage/stubs/DlangSingleImportStub.java
+++ src/main/java/io/github/intellij/dlanguage/stubs/DlangSingleImportStub.java
@@ -8,39 +8,51 @@ import io.github.intellij.dlanguage.resolve.processors.parameters.DAttributes;
 import java.util.HashSet;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class DlangSingleImportStub extends DNamedStubBase<DlangSingleImport> {
 
     private final int numBinds;
     private final Set<String> binds = new HashSet<>();
-    private final @NotNull
-    String importedModule;
+    @NotNull
+    private final String importedModule;
     private final boolean hasName;
     private final String importName;
 
-    public DlangSingleImportStub(final StubElement parent, final IStubElementType elementType,
-        final String name, final int numBinds, final Set<StringRef> binds,
-        final @NotNull String importedModule, final boolean hasName, final String importName,
-        final DAttributes attributes) {
+    public DlangSingleImportStub(final StubElement parent,
+                                 @NotNull final IStubElementType elementType,
+                                 @Nullable final String name,
+                                 final int numBinds,
+                                 final Set<StringRef> binds,
+                                 final @NotNull String importedModule,
+                                 final boolean hasName,
+                                 final String importName,
+                                 final DAttributes attributes) {
         super(parent, elementType, name, attributes);
         this.numBinds = numBinds;
         for (final StringRef bind : binds) {
             this.binds.add(bind.getString());
         }
         this.importedModule = importedModule;
-        if (importedModule.equals(""))
-            throw new IllegalStateException();
+        if ("".equals(importedModule)) {
+            throw new IllegalStateException("importedModule should not be blank");
+        }
         this.hasName = hasName;
         this.importName = importName;
     }
 
-    public DlangSingleImportStub(final StubElement parent, final IStubElementType elementType,
-        final StringRef name, final int numBinds, final Set<StringRef> binds,
-        final @NotNull StringRef importName, final boolean hasName, final StringRef importedModule,
-        final DAttributes attributes) {
-        this(parent, elementType, name.getString(), numBinds, binds, importedModule.getString(),
-            hasName, importName.getString(),
-            attributes);
+    public DlangSingleImportStub(final StubElement parent,
+                                 @NotNull final IStubElementType elementType,
+                                 @Nullable final StringRef name,
+                                 final int numBinds,
+                                 final Set<StringRef> binds,
+                                 final @NotNull StringRef importName,
+                                 final boolean hasName,
+                                 final StringRef importedModule,
+                                 final DAttributes attributes) {
+        this(parent, elementType, name != null ? name.getString() : null,
+            numBinds, binds, importedModule.getString(), hasName,
+            importName.getString(), attributes);
     }
 
     public int numBinds() {
@@ -51,8 +63,8 @@ public class DlangSingleImportStub extends DNamedStubBase<DlangSingleImport> {
         return binds;
     }
 
-    public @NotNull
-    String getImportedModule() {
+    @NotNull
+    public String getImportedModule() {
         return importedModule;
     }
 
