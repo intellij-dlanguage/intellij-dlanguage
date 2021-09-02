201dcc17e282d6a335c970beda6ef310620617db
SingingBush <singingbush@hotmail.com>
2021-09-02 03:03:38 +0100

    tweaked the DCD server test to use LightDlangTestCase with mocked SDK

--- src/test/java/io/github/intellij/dlanguage/codeinsight/dcd/DCDCompletionServerTest.java
+++ src/test/java/io/github/intellij/dlanguage/codeinsight/dcd/DCDCompletionServerTest.java
@@ -1,17 +1,21 @@
 package io.github.intellij.dlanguage.codeinsight.dcd;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.testFramework.LightPlatformTestCase;
+import io.github.intellij.dlanguage.LightDlangTestCase;
 
-public class DCDCompletionServerTest extends LightPlatformTestCase {
+public class DCDCompletionServerTest extends LightDlangTestCase {
 
     private DCDCompletionServer dcd;
 
-    public void testBuildDcdCommand() {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
         this.dcd = new DCDCompletionServer(this.getModule());
+    }
 
+    public void testBuildDcdCommand() {
         final GeneralCommandLine cmd = this.dcd.buildDcdCommand("dcd-server");
 
-        assertEquals("dcd-server -I /src", cmd.getCommandLineString());
+        assertEquals("dcd-server -I /src -I /phobos -I /druntime", cmd.getCommandLineString());
     }
 }
