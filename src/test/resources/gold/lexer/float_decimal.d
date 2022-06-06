dda75fc75e9a4e7696851c37c75b36601e48fa9b
Etienne Brateau <etienne.brateau@gmail.com>
2022-06-06 23:43:29 +0200

    #701 fix lexing of numeric literals

--- src/test/resources/gold/lexer/float_decimal.d
+++ src/test/resources/gold/lexer/float_decimal.d
@@ -4,4 +4,9 @@ void main(){
   float a = 1.2;
   float b = 1.;
   float c = 4._a;//should not be all one float literal
+  float d = 2.645_751;
+  float e = 6.022140857E+23;
+  float f = 6_022.140857E+20;
+  float g = 6_022_.140_857E+20_;
+  float h = 1_000_000.12;
 }
