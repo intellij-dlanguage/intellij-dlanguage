875542d593ad1c5ad68c4f6071ec28fd890f88b3
Etienne Brateau <etienne.brateau@gmail.com>
2022-06-06 23:43:29 +0200

    #701 fix lexing of numeric literals

--- src/test/resources/gold/lexer/float_hex.d
+++ src/test/resources/gold/lexer/float_hex.d
@@ -1,5 +1,8 @@
 import std.stdio;
 
 void main(){
-  float a = 0x1.f;
+  float a = 0x1f;
+  float b = 0x1.FFFFFFFFFFFFFp1023; // double.max
+  float c = 0x1p-52;                // double.epsilon
+  float d = 1.175494351e-38F;       // float.min
 }
