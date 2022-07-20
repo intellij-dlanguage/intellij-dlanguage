39c5fc2040d5d4fe250be9350117d860aa19ad88
etienne02 <etienne02@users.noreply.github.com>
2022-07-20 20:30:52 +0200

    Fix annotation ending when string delimiter has a string postfix (#753)
    Previously, the implementation was blindly considering that the last
    character was always '"'. It’s actually a wrong assumption as a string
    postfix can be added (c, w, d). The code now consider this and the error
    annotation won’t highlight the '"' character even with postfix.
    
    Co-authored-by: Etienne Brateau <etienne.brateau@gmail.com>

--- src/test/resources/gold/highlighting/annotator/invalid_string_delimiters.d
+++ src/test/resources/gold/highlighting/annotator/invalid_string_delimiters.d
@@ -6,3 +6,4 @@ enum a = <error descr="Delimiter cannot be whitespace">q"   test    "</error>;
 enum a = <error descr="Invalid string delimiter">q"test"</error>;
 enum a = <error descr="Invalid string delimiter">q"ùtestù"</error>;
 enum a = q"/test/<error descr="Illegal text found after closing delimiter, expected \" character instead">foo/</error>";
+enum a = q"/test/<error descr="Illegal text found after closing delimiter, expected \" character instead">foo/</error>"d;
