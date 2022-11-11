a926c0aea3fa988d8542455260d8018d43d94fb8
Etienne Brateau <etienne.brateau@gmail.com>
2022-11-11 17:17:32 +0100

    Force colon in case statement to be just after the value
    transform:
    `case 1
    :`
    into:
    `case 1:`

--- src/main/kotlin/io/github/intellij/dlanguage/features/formatter/impl/spacing.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/features/formatter/impl/spacing.kt
@@ -10,6 +10,7 @@ fun createSpacingBuilder(settings: CodeStyleSettings): SpacingBuilder {
             //.aroundInside(OP_COLON, IMPORT_DECLARATION).spaces(1) // import std.stdio : stderr, writeln;
             //.afterInside(COMMA, IMPORT_DECLARATION).spaces(1) // import std.stdio : stderr, writeln;
             .between(KW_CASE, ARGUMENT_LIST).spaces(1)
+            .between(ARGUMENT_LIST, CASE_STATEMENT).spacing(0, 0, 0, false, 0)
             .before(OP_COMMA).spaceIf(false)
             .after(OP_COMMA).spaceIf(true)
             .before(OP_SCOLON).spaceIf(false)
