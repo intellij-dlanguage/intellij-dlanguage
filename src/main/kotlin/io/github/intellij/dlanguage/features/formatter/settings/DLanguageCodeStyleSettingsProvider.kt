61b489ef8df857760cb7a79aa188f4ecf2695201
Etienne Brateau <etienne.brateau@gmail.com>
2022-11-11 16:52:44 +0100

    Clean formatter structure

--- src/main/kotlin/io/github/intellij/dlanguage/features/formatter/settings/DLanguageCodeStyleSettingsProvider.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/features/formatter/settings/DLanguageCodeStyleSettingsProvider.kt
@@ -1,37 +1,40 @@
 package io.github.intellij.dlanguage.features.formatter.settings
 
+import com.intellij.application.options.CodeStyleAbstractConfigurable
+import com.intellij.application.options.CodeStyleAbstractPanel
 import com.intellij.application.options.IndentOptionsEditor
 import com.intellij.application.options.SmartIndentOptionsEditor
 import com.intellij.lang.Language
-import com.intellij.psi.codeStyle.CommonCodeStyleSettings
-import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
+import com.intellij.psi.codeStyle.*
 import io.github.intellij.dlanguage.DLanguage
 
 class DLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
 
-
     companion object {
+        @org.intellij.lang.annotations.Language("D")
         private const val DEFAULT_CODE_SAMPLE =
-            "module example;\n" +
-                "\n" +
-                "import std.stdio;\n" +
-                "\n" +
-                "void main() {\n" +
-                "\twriteln(\"Hello\");\n" +
-                "}"
+            """ module foo.bar.example;
+
+                import std.stdio : writeln;
+
+                void main() {
+                   writeln("Hello");
+                }
+            """
     }
 
-    override fun getLanguage(): Language {
-        return DLanguage
-    }
+    override fun getLanguage(): Language = DLanguage
 
-    override fun getCodeSample(settingsType: SettingsType): String {
-        return DEFAULT_CODE_SAMPLE
-    }
+    override fun getCodeSample(settingsType: SettingsType): String = DEFAULT_CODE_SAMPLE.trimIndent()
 
-    override fun getIndentOptionsEditor(): IndentOptionsEditor {
-        return SmartIndentOptionsEditor()
-    }
+    override fun getIndentOptionsEditor(): IndentOptionsEditor = SmartIndentOptionsEditor()
+
+    override fun createConfigurable(baseSettings: CodeStyleSettings,
+                                    modelSettings: CodeStyleSettings): CodeStyleConfigurable =
+        object : CodeStyleAbstractConfigurable(baseSettings, modelSettings, configurableDisplayName) {
+            override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel =
+                DCodeStyleMainPanel(currentSettings, settings)
+        }
 
     override fun customizeDefaults(commonSettings: CommonCodeStyleSettings,
                                    indentOptions: CommonCodeStyleSettings.IndentOptions) {
@@ -44,6 +47,21 @@ class DLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
         commonSettings.LINE_COMMENT_AT_FIRST_COLUMN = false
 
         super.customizeDefaults(commonSettings, indentOptions)
+    }
+
+    override fun customizeSettings(consumer: CodeStyleSettingsCustomizable, settingsType: SettingsType) {
+        when(settingsType) {
+            SettingsType.SPACING_SETTINGS -> {
+                consumer.showAllStandardOptions()
+            }
+            SettingsType.WRAPPING_AND_BRACES_SETTINGS -> {
+            }
+            SettingsType.BLANK_LINES_SETTINGS -> {
+            }
+            SettingsType.COMMENTER_SETTINGS -> {
+            }
+            else -> consumer.showStandardOptions()
+        }
     }
 
 }
