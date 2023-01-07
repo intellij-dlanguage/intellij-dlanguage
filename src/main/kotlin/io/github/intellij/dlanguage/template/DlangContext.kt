ef947aa62b2298c7284b1cc9ede9124a23d686a8
Etienne Brateau <etienne.brateau@gmail.com>
2023-01-07 18:26:35 +0100

    Remove a deprecation message about live templates
    And resolve a TODO

--- src/main/kotlin/io/github/intellij/dlanguage/template/DlangContext.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/template/DlangContext.kt
@@ -1,13 +1,14 @@
 package io.github.intellij.dlanguage.template
 
+import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtilCore
+import io.github.intellij.dlanguage.DLanguage
 
 open class DlangContext protected constructor() : TemplateContextType("D", "D Language") {
 
-    override fun isInContext(file: PsiFile, offset: Int): Boolean {
-        // Todo: in never IDE's (2018.2.x) the suffix check does not work. does the baseLanguage check superseeds this?
-        return (file.name.endsWith(".d")
-            || file.viewProvider.baseLanguage == io.github.intellij.dlanguage.DLanguage)
+    override fun isInContext(context: TemplateActionContext): Boolean {
+        return PsiUtilCore.getLanguageAtOffset(context.file, context.startOffset).isKindOf(DLanguage)
     }
 }
