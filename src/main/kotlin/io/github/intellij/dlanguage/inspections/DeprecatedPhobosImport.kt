bf369f02d015d9fd87a92cc175827fc39d8eb18e
singingbush <singingbush@hotmail.com>
2023-02-04 18:57:36 +0000

    #840 add localInspection for the use of old phobos modules

---
+++ src/main/kotlin/io/github/intellij/dlanguage/inspections/DeprecatedPhobosImport.kt
@@ -0,0 +1,32 @@
+package io.github.intellij.dlanguage.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import io.github.intellij.dlanguage.DlangBundle
+import io.github.intellij.dlanguage.psi.DlangVisitor
+import io.github.intellij.dlanguage.psi.impl.DLanguageImportDeclarationImpl
+import io.github.intellij.dlanguage.psi.named.DlangSingleImport
+
+class DeprecatedPhobosImport : LocalInspectionTool() {
+    override fun getDescriptionFileName(): String = "DeprecatedPhobosImport.html"
+    override fun getDisplayName(): String = "Deprecated Phobos import"
+    override fun getGroupDisplayName(): String = DlangBundle.message("d.inspections.groupname")
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): DlangVisitor = DeprecatedPhobosImportVisitor(holder)
+}
+
+class DeprecatedPhobosImportVisitor(val holder: ProblemsHolder) : DlangVisitor() {
+    override fun visitImportDeclaration(declaration: DLanguageImportDeclarationImpl) {
+        declaration.singleImports.forEach {
+            when (it.text) {
+                "std.experimental.checkedint" -> register(it, "std.experimental.checkedint became std.checkedint in D 2.099")
+                "std.experimental.logger" -> register(it, "std.experimental.logger moved to std.logger in D 2.100")
+                "std.xml" -> register(it, "std.xml is due to be removed from phobos in 2.101 as it's considered out-dated and not up to Phobos' standards")
+            }
+        }
+    }
+
+    fun register(dlangSingleImport: DlangSingleImport, descriptionTemplate: String) {
+        holder.registerProblem(dlangSingleImport, descriptionTemplate, ProblemHighlightType.LIKE_DEPRECATED)
+    }
+}
