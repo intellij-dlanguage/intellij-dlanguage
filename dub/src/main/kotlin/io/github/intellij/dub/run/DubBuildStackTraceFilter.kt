c7d6fcb2211c0c26090d06335909a0e601e9da20
Etienne Brateau <etienne.brateau@gmail.com>
2023-09-15 15:01:00 +0200

    Fix dub stack trace analyzer file detection
    Now dub may emit a \n at the end of each line, previously the files
    would not have been detected. Moreover, the offset for start and end
    were wrongly calculated.
    
    This commit also improve tests for this component.

--- dub/src/main/kotlin/io/github/intellij/dub/run/DubBuildStackTraceFilter.kt
+++ dub/src/main/kotlin/io/github/intellij/dub/run/DubBuildStackTraceFilter.kt
@@ -1,13 +1,10 @@
 package io.github.intellij.dub.run
 
 import com.intellij.execution.filters.*
-import com.intellij.openapi.editor.markup.EffectType
-import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
 import java.awt.Color
-import java.awt.Font
-import java.lang.NumberFormatException
+import kotlin.NumberFormatException
 
 class DubConsoleFilterProvider : ConsoleFilterProvider {
     override fun getDefaultFilters(project: Project): Array<Filter> = arrayOf(DubBuildSourceFileFilter(project))
@@ -26,13 +23,13 @@ class DubBuildSourceFileFilter(val project: Project) : Filter {
         //     source\package\app.di(53,31): Some Log output
         //     source\package\app.di Some Log output
         //     source\path\app.d:21 Some text output
-        val D_SOURCE_PATH_FORMAT = Regex("^(.*\\.di?):?(\\(\\d+,\\d+\\)|\\d+)?:?\\s+(.*)\$")
+        val D_SOURCE_PATH_FORMAT = Regex("""^(.*\.di?):?(\(\d+,\d+\)|\d+)?:?\s+(.*\n?)$""")
 
         private val BLUE = Color(39, 89, 230)
     }
 
     override fun applyFilter(line: String, entireLength: Int): Filter.Result? {
-        if((line.startsWith("source") || line.startsWith("src")) && line.matches(D_SOURCE_PATH_FORMAT)) {
+        if(line.matches(D_SOURCE_PATH_FORMAT)) {
             // then it's fairly certain we can get a hyperlink to the source file and possibly the line number
             val groups = line.lineSequence()
                 .flatMap { D_SOURCE_PATH_FORMAT.find(it)?.groupValues ?: emptyList() }
@@ -44,12 +41,13 @@ class DubBuildSourceFileFilter(val project: Project) : Filter {
                 .findFileByPath(project.basePath.plus("/").plus(groups[0].replace("\\", "/")))
 
             // negate 1 from the line number as IDEA does line numbers starting from
-            val lineNumber = if(groups[1].isNotBlank()) groups[1].parseLineNumber() -1 else 0
+            val lineNumber = if(groups[1].isNotBlank()) groups[1].parseLineNumber() - 1 else 0
+            val columnNumber = if(groups[1].isNotBlank()) groups[1].parseColumnNumber() - 1 else 0
 
+            val offset = entireLength - line.length
             file?.let {
-                return Filter.Result(0, groups[0].length,
-                    OpenFileHyperlinkInfo(project, it, lineNumber),
-                    TextAttributes(BLUE, null, null, EffectType.BOLD_LINE_UNDERSCORE, Font.ITALIC)
+                return Filter.Result(offset, offset + groups[0].length,
+                    OpenFileHyperlinkInfo(project, it, lineNumber, columnNumber),
                 )
             }
         }
@@ -62,7 +60,7 @@ private fun String.parseLineNumber(): Int {
         this.substringAfter("(")
             .substringBefore(")")
             .split(",").
-            first() // when the test is "(23:25)" it's line number followed by column. We don't need the column
+            first() // when the test is "(23,25)" it's line number followed by column. We don't need the column
     else
         this
 
@@ -73,3 +71,17 @@ private fun String.parseLineNumber(): Int {
     }
 }
 
+private fun String.parseColumnNumber(): Int {
+    val number = if (this.startsWith("("))
+        this.substringAfter("(")
+            .substringBefore(")")
+            .split(",")[1] // when the test is "(23,25)" it's line number followed by column. We need the column
+    else
+        return 0
+
+    return try {
+        Integer.parseInt(number)
+    } catch (e: NumberFormatException) {
+        0 // zeros is fin as default line number
+    }
+}
