77a47f0cdbd270eeb509b00971cc1e4277a46569
Etienne Brateau <etienne.brateau@gmail.com>
2023-09-15 15:01:00 +0200

    Fix dub stack trace analyzer file detection
    Now dub may emit a \n at the end of each line, previously the files
    would not have been detected. Moreover, the offset for start and end
    were wrongly calculated.
    
    This commit also improve tests for this component.

--- dub/src/test/kotlin/io/github/intellij/dub/run/DubBuildSourceFileFilterTest.kt
+++ dub/src/test/kotlin/io/github/intellij/dub/run/DubBuildSourceFileFilterTest.kt
@@ -6,6 +6,16 @@ import junit.framework.TestCase
 class DubBuildSourceFileFilterTest : LightPlatformTestCase() {
 
     @Throws(Exception::class)
+    fun `test line match correctly matching`() {
+        TestCase.assertTrue("source\\package\\app.d Some Log output".matches(DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT))
+        // Windows separators
+        TestCase.assertTrue("source\\package\\app.d Some Log output\n".matches(DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT))
+        // Unix separators
+        TestCase.assertTrue("source/package/app.d Some Log output\n".matches(DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT))
+
+    }
+
+    @Throws(Exception::class)
     fun `test the regex for splitting a D source path, line no, and message`() {
         TestCase.assertEquals("source\\package\\app.d",
             DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find("source\\package\\app.d Some Log output")!!.groupValues[1]
@@ -66,13 +76,16 @@ class DubBuildSourceFileFilterTest : LightPlatformTestCase() {
 
         val filter = DubBuildSourceFileFilter(project)
 
-        val result = filter.applyFilter(line, line.length)
-
-        TestCase.assertNull(result) // WILL BE NULL DUE TO STATIC METHODS IN CODE BASE
-        //TestCase.assertNotNull(result)
-        //val resultItem = result?.resultItems?.get(0)!!
-        //TestCase.assertEquals(0, resultItem.getHighlightStartOffset())
-        //TestCase.assertEquals(31, resultItem.getHighlightEndOffset())
+        assertNoThrowable{filter.applyFilter(line, line.length)}
+        TestCase.assertEquals("source\\config\\database.d",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[1]
+        )
+        TestCase.assertEquals("(53,31)",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[2]
+        )
+        TestCase.assertEquals("blah blah blah, some other stuff",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[3]
+        )
     }
 
     @Throws(Exception::class)
@@ -81,13 +94,16 @@ class DubBuildSourceFileFilterTest : LightPlatformTestCase() {
 
         val filter = DubBuildSourceFileFilter(project)
 
-        val result = filter.applyFilter(line, line.length)
-
-        TestCase.assertNull(result) // WILL BE NULL DUE TO STATIC METHODS IN CODE BASE
-        //TestCase.assertNotNull(result)
-        //val resultItem = result?.resultItems?.get(0)!!
-        //TestCase.assertEquals(0, resultItem.getHighlightStartOffset())
-        //TestCase.assertEquals(31, resultItem.getHighlightEndOffset())
+        assertNoThrowable{filter.applyFilter(line, line.length)}
+        TestCase.assertEquals("source/app.d",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[1]
+        )
+        TestCase.assertEquals("21",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[2]
+        )
+        TestCase.assertEquals("_D3std9exception__T7bailOutHTC9ExceptionZQwFNaNfAyamMAxaZNn [0x10c0aa25a]",
+            DubBuildSourceFileFilter.D_SOURCE_PATH_FORMAT.find(line)!!.groupValues[3]
+        )
     }
 
     @Throws(Exception::class)
