d348b31205bd8dc6510de93be3ffce91f5ed2c17
Etienne Brateau <etienne.brateau@gmail.com>
2023-09-20 19:21:37 +0200

    Fix the dub compile annotation not being displayed

--- dub/src/main/kotlin/io/github/intellij/dub/highlighting/annotation/DubSyntaxAnnotator.kt
+++ dub/src/main/kotlin/io/github/intellij/dub/highlighting/annotation/DubSyntaxAnnotator.kt
@@ -1,11 +1,11 @@
 package io.github.intellij.dub.highlighting.annotation
 
+import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.ExternalAnnotator
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.psi.PsiFile
 import io.github.intellij.dlanguage.highlighting.annotation.DProblem
 
@@ -19,6 +19,15 @@ class DubSyntaxAnnotator : ExternalAnnotator<PsiFile, DubSyntaxAnnotator.State>(
         collectInformation(file)
 
     override fun collectInformation(file: PsiFile): PsiFile = file
+
+    override fun apply(file: PsiFile, annotationResult: State?, holder: AnnotationHolder) {
+        super.apply(file, annotationResult, holder)
+        annotationResult?:return
+        if (annotationResult.annotations.isEmpty() || !file.isValid) return
+        for (annotation in annotationResult.annotations) {
+            annotation.createAnnotations(file, holder)
+        }
+    }
 
     override fun doAnnotate(collectedInfo: PsiFile?): State? {
         collectedInfo ?: return null
