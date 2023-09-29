92180e6dec777d6b6dc9e4243d7113f492e448b0
etienne02 <etienne02@users.noreply.github.com>
2023-09-29 09:22:28 +0200

    Improve documentation links (#900)
    * Fix a potential crash in the doc parser
    
    When the last marker is already a link and the current thing is not a
    link, we wrongly assumed it was one.
    
    * Improve ddoc rendering, add proper support for links
    
    ---------
    
    Co-authored-by: Etienne Brateau <etienne.brateau@gmail.com>

---
+++ src/test/resources/gold/ddoc/parser/wrongImageDetection.d
@@ -0,0 +1,6 @@
+/*****************************************
+ * Link [inline Link](https://test)
+ * follow by non link (x; AliasSeq!(1, 2, 3, 4)) { ... }
+ * should not make the parser crash
+ */
+int declaration;
