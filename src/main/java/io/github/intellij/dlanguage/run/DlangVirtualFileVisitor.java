287918605279d9097c082e3b1a11600ccd337574
singingbush <singingbush@hotmail.com>
2023-10-02 16:23:20 +0100

    867: minor tweaks

--- src/main/java/io/github/intellij/dlanguage/run/DlangVirtualFileVisitor.java
+++ src/main/java/io/github/intellij/dlanguage/run/DlangVirtualFileVisitor.java
@@ -20,14 +20,21 @@ public class DlangVirtualFileVisitor extends VirtualFileVisitor<Void> {
     private static final Logger log = Logger.getInstance(DlangVirtualFileVisitor.class);
 
     private final List<String> dLangSources;
-    private final VirtualFile[] excludedRoots;
+    private final @Nullable VirtualFile[] excludedRoots;
 
 
-    public DlangVirtualFileVisitor(final VirtualFile[] excludedRoots) {
+    public DlangVirtualFileVisitor(@Nullable final VirtualFile[] excludedRoots) {
         this.dLangSources = new ArrayList<>();
         this.excludedRoots = excludedRoots;
     }
 
+    /**
+     * Simple visiting method.
+     * On returning {@code true} a visitor will proceed to file's children, on {@code false} - to file's next sibling.
+     *
+     * @param file a file to visit.
+     * @return {@code true} to proceed to file's children, {@code false} to skip to file's next sibling.
+     */
     @Override
     public boolean visitFile(@NotNull final VirtualFile file) {
         if (!file.isDirectory() && ("d".equals(file.getExtension()) || "di".equals(file.getExtension())) && !isExcluded(file)) {
@@ -45,7 +52,7 @@ public class DlangVirtualFileVisitor extends VirtualFileVisitor<Void> {
             boolean notDlangSdk = !(sdk != null && (sdk.getSdkType() instanceof DlangSdkType));
 
             if(notDlangSdk) {
-                log.debug(String.format("Visited file '%s' but excluding as project sdk is not a D lang SDK", srcFile.getName()));
+                log.debug(String.format("Visited file '%s' but excluding as project sdk (%s) is not a D lang SDK", srcFile.getName(), sdk != null ? sdk.getSdkType() : null));
             }
         }
 
