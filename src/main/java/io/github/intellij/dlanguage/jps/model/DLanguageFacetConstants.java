f70d3b03ed8d81fd7873cbb72c0b9cf6992facbb
Etienne Brateau <etienne.brateau@gmail.com>
2023-10-15 12:31:43 +0200

    Remove JPS code
    This code is not used anymore, and JPS model is deprecated since
    supereseded by Workspace Model.

--- src/main/java/io/github/intellij/dlanguage/jps/model/DLanguageFacetConstants.java
+++
@@ -1,9 +0,0 @@
-package io.github.intellij.dlanguage.jps.model;
-
-public class DLanguageFacetConstants {
-    public static final String ID = "DLanguage";
-    public static final String NAME = "DLanguage";
-
-    private DLanguageFacetConstants() {
-    }
-}
