c24cbfd543ee5b1e23640acc729f2f1d4cdf75d1
Etienne Brateau <etienne.brateau@gmail.com>
2024-05-01 14:22:05 +0200

    Introduce a submodule for the plugin implementation
    This allow to break the current circular dependency between dub and the
    core plugin. A long term plan is to split the core implementation into
    more submodules, implementation that need to remain private (for the
    plugin only) and the api that is shared and can be used by other
    modules. A more complex architecture (with more isolated submodules)
    can be made in the future but the goal of this commit is to be able to
    upgrade intellij-gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,11 +90,7 @@ allprojects {
 }
 
 intellij {
-    pluginName.set("intellij-dlanguage")
     version.set(ideaVersion)
-    //type.set("CL") // maybe helpful to change this when trying other IDE. can be "IC", "IU", "JPS", or "CL"
-    updateSinceUntilBuild.set(false)
-    downloadSources.set(!System.getenv().containsKey("CI") && "IC" == type.get()) // Only download sources when type is Intellij (required because no public source code is available for CLion)
 
     val pluginList = mutableListOf(
         "org.intellij.intelliLang",
@@ -119,109 +115,21 @@ intellij {
     }
     plugins.set(pluginList)
 
-    // uncomment to test/run/debug the plugin with another IDE such as CLion or AppCode
-    //localPath = "<path to>/CLion.app"
-    //localPath = "C:\\Users\\USERNAME\\AppData\\Local\\JetBrains\\Toolbox\\apps\\CLion\\ch-0\\222.3345.126"
-    //localPath = "$System.env.HOME/.local/share/JetBrains/Toolbox/apps/CLion/ch-0/222.3345.126"
-    //localPath = "$System.env.HOME/.local/share/JetBrains/Toolbox/apps/Rider/ch-0/222.3739.37"
-
     tasks {
-        buildPlugin { enabled = true }
-        downloadRobotServerPlugin { enabled = true }
-        jarSearchableOptions { enabled = true }
-        patchPluginXml { enabled = true }
-        //listProductsReleases { enabled = false }
-        prepareSandbox { enabled = true }
-        prepareTestingSandbox { enabled = true }
-        prepareUiTestingSandbox { enabled = true }
-        runIdeForUiTests { enabled = true }
-        runPluginVerifier { enabled = true }
-        signPlugin { enabled = true }
-        verifyPlugin { enabled = true }
-
-        buildSearchableOptions {
-            // workaround for https://youtrack.jetbrains.com/issue/IDEA-210683
-            jvmArgs = listOf(
-                "--illegal-access=warn",
-                "--add-opens=java.base/java.lang=ALL-UNNAMED",
-                "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",
-                "--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED", // Linux only
-                "--add-opens=java.desktop/sun.font=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.swing=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt.event=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED",
-                "--add-opens=java.desktop/com.sun.java.swing.plaf.gtk=ALL-UNNAMED", // Linux only
-                "--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED",  // Mac OSX only
-            )
-            enabled = false // workaround for https://youtrack.jetbrains.com/issue/KT-42837
-        }
-
-        runIde {
-            enabled = true
-            jvmArgs = listOf(
-                "--illegal-access=warn",
-                "--add-opens=java.base/java.lang=ALL-UNNAMED",
-                "--add-opens=java.base/java.nio=ALL-UNNAMED",
-                "--add-opens=java.base/java.util=ALL-UNNAMED",
-                "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",
-                "--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED", // Linux only
-                "--add-opens=java.desktop/sun.font=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.swing=ALL-UNNAMED",
-                "--add-opens=java.desktop/sun.java2d=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED",
-                "--add-opens=java.desktop/java.awt.event=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED",
-                "--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED",
-                "--add-opens=java.desktop/com.sun.java.swing.plaf.gtk=ALL-UNNAMED", // Linux only
-                "--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED",  // Mac OSX only
-            )
-            systemProperty("idea.debug.mode", "true")
-            // The package names have to be prefixed with a #
-            systemProperty("idea.log.debug.categories", "#io.github.intellij.dlanguage")
-            //systemProperty("idea.log.trace.categories"], "#io.github.intellij.dlanguage")
-
-            // Enable internal mode (https://plugins.jetbrains.com/docs/intellij/enabling-internal.html)
-            systemProperty("idea.is.internal", "true")
-
-            // idea.auto.reload.plugins=false
-            systemProperty("ide.show.tips.on.startup.default.value", "false")
-
-            // Set these properties to test alternative languages
-            // systemProperty("user.language"], "de")
-            // systemProperty("user.country"], "DE")
-
-            // jbrVersion = null
-            // jbrVersion = "11_0_4b304.69"
-        }
-
-        publishPlugin {
-            enabled = true
-            token.set(provider {
-                System.getenv("JETBRAINS_TOKEN")
-            })
-            channels.set(publishChannels)
-        }
-    }
-}
-
-
-// take the version number defined in gradle build and use that in plugin.xml
-tasks.register<Copy>("initConfig") {
-    from("src/main/resources") {
-        include("**/plugin.xml")
-        expand("version" to version)
+        buildPlugin { enabled = false }
+        buildSearchableOptions { enabled = false }
+        downloadRobotServerPlugin { enabled = false }
+        jarSearchableOptions { enabled = false }
+        patchPluginXml { enabled = false }
+        prepareSandbox { enabled = false }
+        prepareTestingSandbox { enabled = false }
+        prepareUiTestingSandbox { enabled = false }
+        publishPlugin { enabled = false }
+        runIde { enabled = false }
+        runIdeForUiTests { enabled = false }
+        runPluginVerifier { enabled = false }
+        signPlugin { enabled = false }
+        verifyPlugin { enabled = false }
     }
 }
 
@@ -293,7 +201,7 @@ dependencies {
     implementation (project(":errorreporting"))
     implementation (project(":debugger"))
     implementation (project(":sdlang"))
-    runtimeOnly (project(":dub"))
+    testImplementation(project(":dlang:plugin-impl"))
 
     implementation (libs.gson) // used by dub parser
 
