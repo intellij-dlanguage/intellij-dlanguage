159670fb63e72a7ea66b94af04ec60ee332c065f
Etienne Brateau <etienne.brateau@gmail.com>
2024-05-09 18:46:54 +0200

    Convert DReferenceContributor to kotlin

--- src/main/kotlin/io/github/intellij/dlanguage/psi/references/DReferenceContributor.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/psi/references/DReferenceContributor.kt
@@ -1,22 +1,23 @@
-package io.github.intellij.dlanguage.psi.references;
+package io.github.intellij.dlanguage.psi.references
 
-import com.intellij.patterns.PlatformPatterns;
-import com.intellij.patterns.PsiElementPattern;
-import com.intellij.psi.PsiNamedElement;
-import com.intellij.psi.PsiReferenceContributor;
-import com.intellij.psi.PsiReferenceRegistrar;
-import io.github.intellij.dlanguage.DLanguage;
-import io.github.intellij.dlanguage.psi.named.DlangFunctionDeclaration;
-import io.github.intellij.dlanguage.psi.named.DlangIdentifier;
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceRegistrar
+import io.github.intellij.dlanguage.DLanguage
+import io.github.intellij.dlanguage.psi.named.DlangFunctionDeclaration
+import io.github.intellij.dlanguage.psi.named.DlangIdentifier
 
-public class DReferenceContributor extends PsiReferenceContributor {
-    @Override
-    public void registerReferenceProviders(final PsiReferenceRegistrar registrar) {
-        final PsiElementPattern.Capture<PsiNamedElement> variableCapture =
-            PlatformPatterns.psiElement(PsiNamedElement.class).withParent(DlangIdentifier.class)
-                .withParent(DlangFunctionDeclaration.class).withLanguage(DLanguage.INSTANCE);
-        registrar.registerReferenceProvider(variableCapture,
-            new DReferenceProvider());
+class DReferenceContributor : PsiReferenceContributor() {
+    override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
+        val variableCapture =
+            PlatformPatterns.psiElement(PsiNamedElement::class.java)
+                .withParent(DlangIdentifier::class.java)
+                .withParent(DlangFunctionDeclaration::class.java).withLanguage(DLanguage)
+        registrar.registerReferenceProvider(
+            variableCapture,
+            DReferenceProvider()
+        )
     }
 }
 
