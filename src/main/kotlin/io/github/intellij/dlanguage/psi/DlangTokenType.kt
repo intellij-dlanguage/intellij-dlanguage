3b54545a827b05ea1c3090070862abcf7356b790
Etienne Brateau <etienne.brateau@gmail.com>
2024-05-09 17:33:57 +0200

    Convert DlangTokenType to kotlin

--- src/main/kotlin/io/github/intellij/dlanguage/psi/DlangTokenType.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/psi/DlangTokenType.kt
@@ -1,26 +1,15 @@
-package io.github.intellij.dlanguage.psi;
+package io.github.intellij.dlanguage.psi
+
+import com.intellij.psi.tree.IElementType
+import io.github.intellij.dlanguage.DLanguage
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.NotNull
 
 
-import com.intellij.psi.tree.IElementType;
-import io.github.intellij.dlanguage.DLanguage;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-
-public class DlangTokenType extends IElementType {
-
-    private final String name;
-
-    public DlangTokenType(@NotNull @NonNls final String debugName) {
-        super(debugName, DLanguage.INSTANCE);
-        this.name = debugName;
-    }
-
-    @Override
-    public String toString() {
-        return "DlangTokenType." + super.toString();
-    }
-
-    public String getName() {
-        return name;
+class DlangTokenType(val name: @NonNls String) : IElementType(
+    name, DLanguage
+) {
+    override fun toString(): String {
+        return "DlangTokenType." + super.toString()
     }
 }
