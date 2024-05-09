f9afe5f50d98f5e4db0eb6ff02d677d6a06643cd
Etienne Brateau <etienne.brateau@gmail.com>
2024-05-09 17:37:06 +0200

    Convert DlangElementType to kotlin

--- src/main/kotlin/io/github/intellij/dlanguage/psi/DlangElementType.kt
+++ src/main/kotlin/io/github/intellij/dlanguage/psi/DlangElementType.kt
@@ -1,13 +1,7 @@
-package io.github.intellij.dlanguage.psi;
+package io.github.intellij.dlanguage.psi
 
-import com.intellij.psi.tree.IElementType;
-import io.github.intellij.dlanguage.DLanguage;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.tree.IElementType
+import io.github.intellij.dlanguage.DLanguage
+import org.jetbrains.annotations.NonNls
 
-public class DlangElementType extends IElementType {
-
-    public DlangElementType(@NotNull @NonNls final String debugName) {
-        super(debugName, DLanguage.INSTANCE);
-    }
-}
+class DlangElementType(debugName: @NonNls String) : IElementType(debugName, DLanguage)
