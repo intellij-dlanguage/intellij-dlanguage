1020bc9c63d18971d4f26bf5ba82d9b52fa888cd
Etienne Brateau <etienne.brateau@gmail.com>
2024-09-06 20:34:24 +0200

    #948 Add a test for the static foreach folding

---
+++ src/test/resources/gold/folding/static_foreach.d
@@ -0,0 +1,14 @@
+
+void main() <fold text='{...}'>{
+    version (none) <fold text='{...}'>{
+
+    }</fold>
+
+    static foreach (a; []) <fold text='{...}'>{
+
+    }</fold>
+
+    version (none) <fold text='{...}'>{
+
+    }</fold>
+}</fold>
