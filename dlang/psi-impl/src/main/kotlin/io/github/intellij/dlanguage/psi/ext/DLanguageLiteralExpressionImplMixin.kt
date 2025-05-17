dc3b9433a6d48b9b7056fa99a7a8e5b8c0d51d20
Etienne Brateau <etienne.brateau@gmail.com>
2025-05-17 13:39:45 +0200

    Add full support for integer and float literal for type deduction

--- dlang/psi-impl/src/main/kotlin/io/github/intellij/dlanguage/psi/ext/DLanguageLiteralExpressionImplMixin.kt
+++ dlang/psi-impl/src/main/kotlin/io/github/intellij/dlanguage/psi/ext/DLanguageLiteralExpressionImplMixin.kt
@@ -7,6 +7,7 @@ import io.github.intellij.dlanguage.psi.types.DArrayType
 import io.github.intellij.dlanguage.psi.types.DPrimitiveType
 import io.github.intellij.dlanguage.psi.types.DType
 import io.github.intellij.dlanguage.utils.ClassDeclaration
+import io.github.intellij.dlanguage.utils.DPsiLiteralUtil
 import io.github.intellij.dlanguage.utils.LiteralExpression
 
 abstract class DLanguageLiteralExpressionImplMixin(node: ASTNode) : ASTWrapperPsiElement(node),
@@ -37,18 +38,52 @@ abstract class DLanguageLiteralExpressionImplMixin(node: ASTNode) : ASTWrapperPs
         if (kW_TRUE != null || kW_FALSE != null) {
             return DPrimitiveType.fromText("bool")
         }
-        // TODO note: better way to determine the type of the literal
-        //   is to handle it at the lexer level (distinguish int from long based on the extension)
         if (integeR_LITERAL != null) {
-            // TODO long, uint and ulong
-            return DPrimitiveType.fromText("int")
+            val text = integeR_LITERAL!!.text
+            val intValue = getLongValue(text)
+            if (text.endsWith("uL") || text.endsWith("UL") || text.endsWith("Lu") || text.endsWith("LU")) {
+                return DPrimitiveType.fromText("ulong")
+            } else if (text.endsWith("u") || text.endsWith("U")) {
+                if (intValue <= 4_294_967_295U)
+                    return DPrimitiveType.fromText("uint")
+                return DPrimitiveType.fromText("ulong")
+            } else if (text.endsWith("L")) {
+                if ((DPsiLiteralUtil.isHexadecimal(text) || DPsiLiteralUtil.isBinary(text)) && intValue > DPsiLiteralUtil.SIGNED_LONG_MAX.toULong())
+                    return DPrimitiveType.fromText("ulong")
+                // if a regular long (not binary not hex) is declared a higher limit than 9_223_372_036_854_775_807L,
+                // then it’s an overflow, an error should be reported
+                return DPrimitiveType.fromText("long")
+            } else {
+                // no extension
+                if (intValue <= 2_147_483_647U)
+                    return  DPrimitiveType.fromText("int")
+                // binary and hexadecimal value can be uint even without a suffix
+                if ((DPsiLiteralUtil.isHexadecimal(text) || DPsiLiteralUtil.isBinary(text)) && intValue <= 4_294_967_295U) {
+                    return DPrimitiveType.fromText("uint")
+                }
+                if (intValue <= DPsiLiteralUtil.SIGNED_LONG_MAX.toULong())
+                    return DPrimitiveType.fromText("long")
+                return DPrimitiveType.fromText("ulong")
+            }
         }
         if (floaT_LITERAL != null) {
-            // TODO double, ifloat, idouble, real, ireal
-            return DPrimitiveType.fromText("float")
+            val last = floaT_LITERAL!!.text.last()
+            if (last == 'i') {
+                val preLast = floaT_LITERAL!!.text[floaT_LITERAL!!.text.lastIndex - 1]
+                return when (preLast) {
+                    'f', 'F' -> DPrimitiveType.fromText("ifloat")
+                    'L' -> DPrimitiveType.fromText("ireal")
+                    else -> DPrimitiveType.fromText("idouble")
+                }
+            }
+            return when (last) {
+                'f', 'F' -> DPrimitiveType.fromText("float")
+                'L' -> DPrimitiveType.fromText("real")
+                else -> DPrimitiveType.fromText("double")
+            }
         }
         if (characteR_LITERAL != null) {
-            // TODO dchar and wchar
+            // TODO dchar and wchar (need to check the char value to determine his real type)
             return DPrimitiveType.fromText("char")
         }
         if (alternatE_WYSIWYG_STRINGs.isNotEmpty() || doublE_QUOTED_STRINGs.isNotEmpty() ||
@@ -57,6 +92,39 @@ abstract class DLanguageLiteralExpressionImplMixin(node: ASTNode) : ASTWrapperPs
             return DArrayType(DPrimitiveType.fromText("char"), null) // TODO immutable (and handle wstring and dstring)
         }
         return null
+    }
+
+    private fun getLongValue(text: String): ULong {
+        var finalText = text
+        // remove the suffix, can loop at most twice
+        while (finalText.endsWith("U") || finalText.endsWith("u") || finalText.endsWith("L"))
+            finalText = finalText.substring(0, finalText.length - 1)
+        finalText = finalText.replace("_", "")
+
+        if (DPsiLiteralUtil.isBinary(finalText)) {
+            finalText = finalText.substring(2)
+            return try {
+                finalText.toULong(2)
+            } catch (_: NumberFormatException) {
+                // Number too high probably
+                ULong.MAX_VALUE
+            }
+        }
+        if (DPsiLiteralUtil.isHexadecimal(finalText)) {
+            finalText = finalText.substring(2)
+            return try {
+                finalText.toULong(16)
+            } catch (_: NumberFormatException) {
+                // Number too high probably
+                ULong.MAX_VALUE
+            }
+        }
+        return try {
+            finalText.toULong()
+        } catch (_: java.lang.NumberFormatException) {
+            // Number too high probably
+            ULong.MAX_VALUE
+        }
     }
 
     private fun getSuperType(aClass: ClassDeclaration): DType? {
--- src/test/kotlin/io/github/intellij/dlanguage/types/AutoValueAssignmentTypeTest.kt
+++ src/test/kotlin/io/github/intellij/dlanguage/types/AutoValueAssignmentTypeTest.kt
@@ -12,14 +12,59 @@ class AutoValueAssignmentTypeTest : LightPlatformCodeInsightFixture4TestCase() {
     fun testAutoExpressionLiteralExpressionAssignment() {
         doTest("int", """auto expr = 1;""")
         doTest("float", """auto expr = 1f;""")
-        // TODO
-        //doTest("long", """auto expr = 1L;""")
-        //doTest("ulong", """auto expr = 1UL;""")
-        //doTest("double", """auto expr = 1.0;""")
+        doTest("float", """auto expr = 1F;""")
+        doTest("long", """auto expr = 1L;""")
+        doTest("long", """auto expr = 0b1L;""")
+        doTest("long", """auto expr = 0xFL;""")
+        doTest("ulong", """auto expr = 1UL;""")
+        doTest("double", """auto expr = 1.0;""")
+        doTest("double", """auto expr = 1.;""")
         doTest("float", """auto expr = 1.0f;""")
         doTest("bool", """auto expr = true;""")
         doTest("bool", """auto expr = false;""")
         doTest("char", """auto expr = 'a';""")
+
+        // Check against limit provided in the lexer spec page
+        doTest("int", """auto expr = 0;""")
+        doTest("int", """auto expr = 2_147_483_647;""")
+        doTest("long", """auto expr = 2_147_483_648;""")
+        doTest("long", """auto expr = 9_223_372_036_854_775_807;""")
+        doTest("ulong", """auto expr = 9_223_372_036_854_775_808;""")
+        doTest("ulong", """auto expr = 18_446_744_073_709_551_615;""")
+
+        doTest("long", """auto expr = 0L;""")
+        doTest("long", """auto expr = 9_223_372_036_854_775_807L;""")
+        doTest("uint", """auto expr = 0U;""")
+        doTest("uint", """auto expr = 4_294_967_295U;""")
+        doTest("ulong", """auto expr = 4_294_967_296U;""")
+        doTest("ulong", """auto expr = 18_446_744_073_709_551_615U;""")
+        doTest("ulong", """auto expr = 0UL;""")
+        doTest("ulong", """auto expr = 18_446_744_073_709_551_615UL;""")
+
+        doTest("int", """auto expr = 0x0;""")
+        doTest("int", """auto expr = 0x7FFF_FFFF;""")
+        doTest("uint", """auto expr = 0x8000_0000;""")
+        doTest("uint", """auto expr = 0xFFFF_FFFF;""")
+        doTest("long", """auto expr = 0x1_0000_0000;""")
+        doTest("long", """auto expr = 0x7FFF_FFFF_FFFF_FFFF;""")
+        doTest("ulong", """auto expr = 0x8000_0000_0000_0000;""")
+        doTest("ulong", """auto expr = 0xFFFF_FFFF_FFFF_FFFF;""")
+
+        doTest("long", """auto expr = 0x0L;""")
+        doTest("long", """auto expr = 0x7FFF_FFFF_FFFF_FFFFL;""")
+        doTest("ulong", """auto expr = 0x8000_0000_0000_0000L;""")
+        doTest("ulong", """auto expr = 0xFFFF_FFFF_FFFF_FFFFL;""")
+        doTest("uint", """auto expr = 0x0U;""")
+        doTest("uint", """auto expr = 0xFFFF_FFFFU;""")
+        doTest("ulong", """auto expr = 0x1_0000_0000U;""")
+        doTest("ulong", """auto expr = 0xFFFF_FFFF_FFFF_FFFFU;""")
+        doTest("ulong", """auto expr = 0x0UL;""")
+        doTest("ulong", """auto expr = 0xFFFF_FFFF_FFFF_FFFFUL;""")
+
+        doTest("float", """auto expr = -1.175494351e-38F;""")
+        doTest("double", """auto expr = 0xAp0;""")
+        doTest("double", """auto expr = 0x1.FFFFFFFFFFFFFp1023;""")
+        doTest("double", """auto expr = 0x1p-52;""")
     }
 
     @Test
