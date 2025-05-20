a66e3d34e67ccc0bc38e72000c3a769bf807b9b5
Etienne Brateau <etienne.brateau@gmail.com>
2025-05-20 22:31:36 +0200

    Improve typing for enum members
    Especially anonymous enum members

--- dlang/psi-api/src/main/java/io/github/intellij/dlanguage/psi/named/DLanguageEnumMember.java
+++ dlang/psi-api/src/main/java/io/github/intellij/dlanguage/psi/named/DLanguageEnumMember.java
@@ -3,10 +3,11 @@ package io.github.intellij.dlanguage.psi.named;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.StubBasedPsiElement;
-import io.github.intellij.dlanguage.psi.DLanguageAssignExpression;
 import io.github.intellij.dlanguage.psi.DLanguageEnumMemberAttribute;
 import io.github.intellij.dlanguage.psi.DLanguageType;
 import io.github.intellij.dlanguage.psi.interfaces.DNamedElement;
+import io.github.intellij.dlanguage.psi.interfaces.DTypedElement;
+import io.github.intellij.dlanguage.psi.interfaces.Expression;
 import io.github.intellij.dlanguage.stubs.DLanguageEnumMemberStub;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,7 +15,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 
-public interface DLanguageEnumMember extends PsiElement, DNamedElement,
+public interface DLanguageEnumMember extends PsiElement, DNamedElement, DTypedElement,
     StubBasedPsiElement<DLanguageEnumMemberStub> {
 
     @NotNull
@@ -30,5 +31,5 @@ public interface DLanguageEnumMember extends PsiElement, DNamedElement,
     DLanguageType getType();
 
     @Nullable
-    DLanguageAssignExpression getAssignExpression();
+    Expression getExpression();
 }
