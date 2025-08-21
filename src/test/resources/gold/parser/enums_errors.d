dfdcb3cb50c0c592d9e6cc85b30e5837dfb35cb2
Etienne Brateau <etienne.brateau@gmail.com>
2025-08-21 19:54:13 +0200

    Fix parsing of invalid enum members
    Found by opening libdparse, a test with two successive commas were
    turning the parser in infinite loop.

--- src/test/resources/gold/parser/enums_errors.d
+++ src/test/resources/gold/parser/enums_errors.d
@@ -1,8 +1,27 @@
 
+// invalid type declared in enum member
 enum X {
     int a
 }
 
+// invalid type declared in enum member
 enum : long {
     short a = 3
+}
+
+// 2 successive commas
+enum A {
+    a,,
+}
+
+// deprecated with no name
+enum A {
+	a,
+	deprecated,
+}
+
+// attribute with no name
+enum A {
+	c,
+	@test,
 }
