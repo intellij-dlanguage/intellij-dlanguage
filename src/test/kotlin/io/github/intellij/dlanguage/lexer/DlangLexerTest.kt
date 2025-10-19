89390a15c72831603c49e9b532be38a5406e3a6a
Etienne Brateau <etienne.brateau@gmail.com>
2025-10-19 17:10:14 +0200

    Remove a lexer test
    This test covers a full D std file, it is useless to cover a such
    complex file to just know how are handled tokens. It tends to just
    accept whatever the lexer generate without actually check.
    
    Lexer for D Language is simple, it is easy to covers the full accepted
    range of tokens without having to cover a full complex file.
    
    This kind of file is more useful for parser.

--- src/test/kotlin/io/github/intellij/dlanguage/lexer/DlangLexerTest.kt
+++ src/test/kotlin/io/github/intellij/dlanguage/lexer/DlangLexerTest.kt
@@ -106,6 +106,10 @@ class DlangLexerTest : DlangLexerTestBase("lexer") {
     fun testcomment_unclosed() = doTest("/* unclosed comment", "DlangTokenType.BLOCK_COMMENT ('/* unclosed comment')")
 
 
+    //chars
+    fun testchar() = doTest()
+
+
     // floats
     fun testfloat_decimal() = doTest()
     fun testfloat_hex()     = doTest()
@@ -195,12 +199,6 @@ BAD_CHARACTER ('q"/test"d;')
     fun testtokens_unordered_le()         = doTest()
     fun testtokens_xor()                  = doTest()
     fun testtokens_xor_assign()           = doTest()
-
-    //chars
-    fun testchar() = doTest()
-
-    //stdlib
-    fun testparallelism() = doTest()
 
     //issue 77
     fun testissue77() = doTest()
