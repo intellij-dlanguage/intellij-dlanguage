a7278abfdae14675052037056bbee23ebcc3e7b8
singingbush <singingbush@hotmail.com>
2026-02-13 00:10:30 +0000

    #1090: Allow mago-mi.exe to be selected on Windows

--- src/main/java/io/github/intellij/dlanguage/settings/DLanguageToolsConfigurable.java
+++ src/main/java/io/github/intellij/dlanguage/settings/DLanguageToolsConfigurable.java
@@ -177,9 +177,9 @@ public class DLanguageToolsConfigurable implements SearchableConfigurable {
                     } else if(!Files.isExecutable(path)) {
                         t.versionField.setText("Path is not executable");
                         t.versionField.setDisabledTextColor(UIManager.getColor("Component.warningFocusColor"));
-                    } else if(!StringUtil.containsIgnoreCase(exePath, t.command)) {
-                        t.versionField.setText(String.format("Not a valid %s binary", t.command));
-                        t.versionField.setDisabledTextColor(UIManager.getColor("Component.warningFocusColor"));
+//                    } else if(!StringUtil.containsIgnoreCase(exePath, t.command)) {
+//                        t.versionField.setText(String.format("Not a valid %s binary", t.command));
+//                        t.versionField.setDisabledTextColor(UIManager.getColor("Component.warningFocusColor"));
                     } else {
                         // then it's a valid path to the tool
                         t.updateVersion();
@@ -298,15 +298,15 @@ public class DLanguageToolsConfigurable implements SearchableConfigurable {
         }
     }
 
-    /**
-     * Ensures that the UI component for selecting a Dub Tool can only be used to select the correct binary
-     */
-    private class DubToolBinaryChooserDescriptor extends FileChooserDescriptor {
-        DubToolBinaryChooserDescriptor(@NotNull final String binaryName) {
-            super(true, false, false, false, false, false);
-            withFileFilter(vf -> vf.getNameWithoutExtension().equalsIgnoreCase(binaryName));
-        }
-    }
+//    /**
+//     * Ensures that the UI component for selecting a Dub Tool can only be used to select the correct binary
+//     */
+//    private class DubToolBinaryChooserDescriptor extends FileChooserDescriptor {
+//        DubToolBinaryChooserDescriptor(@NotNull final String binaryName) {
+//            super(true, false, false, false, false, false);
+//            withFileFilter(vf -> vf.getNameWithoutExtension().equalsIgnoreCase(binaryName));
+//        }
+//    }
 
     /**
      * Manages the group of fields which reside to a particular tool.
@@ -349,12 +349,16 @@ public class DLanguageToolsConfigurable implements SearchableConfigurable {
                 new PropertyField(key.getPathKey(), pathField),
                 new PropertyField(key.getFlagsKey(), flagsField));
 
-            pathField.addBrowseFolderListener(
-                null,
-                new DubToolBinaryChooserDescriptor(command)
-                    .withTitle(String.format("Select %s executable", command))
-                    .withDescription("")
-            );
+            var chooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false)
+                .withTitle(String.format("Select %s executable", command))
+                .withDescription(SystemInfo.isWindows && "gdb".equals(command) ? "On Windows mago-mi.exe can be used" : "")
+                .withShowHiddenFiles(false)
+                .withFileFilter(SystemInfo.isWindows && "gdb".equals(command) ?
+                    vf -> List.of(command, "mago-mi").contains(vf.getNameWithoutExtension()) :
+                    vf -> vf.getNameWithoutExtension().equalsIgnoreCase(command)
+                );
+
+            pathField.addBrowseFolderListener(null, chooserDescriptor);
 
             autoFindButton.addActionListener(new LocateToolListener(pathField, command));
 
@@ -456,7 +460,7 @@ public class DLanguageToolsConfigurable implements SearchableConfigurable {
             if (StringUtil.isNotEmpty(path)) {
                 pathField.setText(StringUtil.trim(path));
             } else {
-                Messages.showErrorDialog(String.format("Could not find '%s'.", command), "DLanguage");
+                Messages.showWarningDialog(String.format("Could not find '%s'.", command), "DLanguage");
             }
         }
 
